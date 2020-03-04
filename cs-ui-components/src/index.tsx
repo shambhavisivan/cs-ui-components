32dc142bb23633e02947c481e5195c85a7d2cd22
kseler <karlo.seler@cloudsensesolutions.com>
2020-03-04 10:07:11 +0100

    Added components modules to index.tsx
    Change-Id: I22bef0a94ab50e9a117be86e3f16d0ac944226ca

--- cs-ui-components/src/index.tsx
+++ cs-ui-components/src/index.tsx
@@ -1,29 +1,42 @@
 import CSAlert from './components/CSAlert';
 import CSButton from './components/CSButton';
 import CSCard from './components/card/CSCard';
+import CSCardBody from './components/card/CSCardBody';
+import CSCardFooter from './components/card/CSCardFooter';
 import CSCardHeader from './components/card/CSCardHeader';
 import CSCheckbox from './components/CSCheckbox';
 import CSChip from './components/CSChip';
 import CSCustomSelect from './components/CSCustomSelect';
 import CSDatepicker from './components/CSDatepicker';
-import CSLabel from './components/CSLabel';
 import CSIcon from './components/CSIcon';
 import CSInputNumber from './components/CSInputNumber';
 import CSInputSearch from './components/CSInputSearch';
 import CSInputText from './components/CSInputText';
+import CSLabel from './components/CSLabel';
 import CSMainHeader from './components/CSMainHeader';
+import CSMainHeaderIcon from './components/CSMainHeaderIcon';
 import CSMainHeaderLeft from './components/CSMainHeaderLeft';
+import CSMainHeaderRight from './components/CSMainHeaderRight';
 import CSModal from './components/CSModal';
+import CSModalBody from './components/CSModalBody';
 import CSModalHeader from './components/CSModalHeader';
 import CSModalFooter from './components/CSModalFooter';
+import CSPagination from './components/CSPagination';
 import CSPaginationWrapper from './components/CSPaginationWrapper';
 import CSPath from './components/CSPath';
+import CSPathItem from './components/CSPathItem';
+import CSPathWrapper from './components/CSPathWrapper';
 import CSProgressBar from './components/CSProgressBar';
 import CSSelect from './components/CSSelect';
 import CSSection from './components/CSSection';
 import CSSpinner from './components/CSSpinner';
-import CSTable from './components/CSTable';
+import CSTab from './components/CSTab';
 import CSTabGroup from './components/CSTabGroup';
+import CSTable from './components/CSTable';
+import CSTableBody from './components/CSTableBody';
+import CSTableCell from './components/CSTableCell';
+import CSTableHeader from './components/CSTableHeader';
+import CSTableRow from './components/CSTableRow';
 import CSTextarea from './components/CSTextarea';
 import CSToast from './components/CSToast';
 import CSToggle from './components/CSToggle';
@@ -35,6 +48,8 @@ export {
 	CSAlert,
 	CSButton,
 	CSCard,
+	CSCardBody,
+	CSCardFooter,
 	CSCardHeader,
 	CSCheckbox,
 	CSChip,
@@ -46,18 +61,29 @@ export {
 	CSInputSearch,
 	CSInputText,
 	CSMainHeader,
+	CSMainHeaderIcon,
 	CSMainHeaderLeft,
+	CSMainHeaderRight,
 	CSModal,
+	CSModalBody,
 	CSModalHeader,
 	CSModalFooter,
+	CSPagination,
 	CSPaginationWrapper,
 	CSPath,
+	CSPathItem,
+	CSPathWrapper,
 	CSProgressBar,
 	CSSelect,
 	CSSection,
 	CSSpinner,
-	CSTable,
+	CSTab,
 	CSTabGroup,
+	CSTable,
+	CSTableBody,
+	CSTableCell,
+	CSTableHeader,
+	CSTableRow,
 	CSTextarea,
 	CSToast,
 	CSToggle,
