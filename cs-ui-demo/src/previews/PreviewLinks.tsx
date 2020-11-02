6e1e20177c17169d10f2d0c8cc79bd8d74f695d6
kseler <karlo.seler@cloudsensesolutions.com>
2020-11-02 13:05:23 +0100

    [cs-ui] PreviewLinks - changed component to make Accessibility, Properties List and API fixed
    Change-Id: Ie1b44ca9a07fbb72553eaada93b0817fc79032f0

--- cs-ui-demo/src/previews/PreviewLinks.tsx
+++ cs-ui-demo/src/previews/PreviewLinks.tsx
@@ -8,49 +8,53 @@ class PreviewLinks extends React.Component<PreviewLinksProps> {
 	render() {
 		return (
 			<>
-				{this.props.component.examples.map((example: any, i: number) => (
-					<div className="prop-group" key={i}>
-						<h4>
-							<a href={`#${this.props.component.name}-${example.propName}`} key={example.propName}>
-								{example.propName}
-							</a>
-						</h4>
-						{example.variations.map((variation: any, j: any) => (
-							variation.quickLink ? (
-								<span key={j}>
-									<a href={`#${this.props.component.name}-${example.propName}-${variation.quickLink}`}>
-										{variation.quickLink}
-									</a>
-								</span>
-							) : null
-						))}
-					</div>
-				))}
-				<div className="prop-group">
-					<h4>
-						<a href={`#properties-table-${this.props.component.name}`}>
-							Properties List
-						</a>
-					</h4>
+				<div className="prop-sidebar-wrapper">
+					{this.props.component.examples.map((example: any, i: number) => (
+						<div className="prop-group" key={i}>
+							<h4>
+								<a href={`#${this.props.component.name}-${example.propName}`} key={example.propName}>
+									{example.propName}
+								</a>
+							</h4>
+							{example.variations.map((variation: any, j: any) => (
+								variation.quickLink ? (
+									<span key={j}>
+										<a href={`#${this.props.component.name}-${example.propName}-${variation.quickLink}`}>
+											{variation.quickLink}
+										</a>
+									</span>
+								) : null
+							))}
+						</div>
+					))}
 				</div>
-				{this.props.component.accessibility && (
+				<div className="prop-sidebar-bottom-group">
 					<div className="prop-group">
 						<h4>
-							<a href={`#accessibility-table-${this.props.component.name}`}>
-								Accessibility
+							<a href={`#properties-table-${this.props.component.name}`}>
+								Properties List
 							</a>
 						</h4>
 					</div>
-				)}
-				{this.props.component.api && (
-					<div className="prop-group">
-						<h4>
-							<a href={`#api-table-${this.props.component.name}`}>
-								API
-							</a>
-						</h4>
-					</div>
-				)}
+					{this.props.component.accessibility && (
+						<div className="prop-group">
+							<h4>
+								<a href={`#accessibility-table-${this.props.component.name}`}>
+									Accessibility
+								</a>
+							</h4>
+						</div>
+					)}
+					{this.props.component.api && (
+						<div className="prop-group">
+							<h4>
+								<a href={`#api-table-${this.props.component.name}`}>
+									API
+								</a>
+							</h4>
+						</div>
+					)}
+				</div>
 			</>
 		);
 	}
