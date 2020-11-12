e4a448fb02ec1fd68906f2f971e7336e0296ec4f
Nikol Badanjak <nikol.badanjak@cloudsensesolutions.com>
2020-11-12 14:23:49 +0100

    [cs-ui] Add getting started page as a default one
    Change-Id: Icf4f435d5b8e9e009a4678f7fb4f90441c74b0fa

--- cs-ui-demo/src/App.tsx
+++ cs-ui-demo/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.scss';
 import React from 'react';
 
-import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom';
+import {BrowserRouter as Router, Route, NavLink, Redirect, Switch} from 'react-router-dom';
 
 import CSComponentsList from './previews/CSComponentsList';
 import CSIconsList from './previews/CSIconsList';
@@ -22,8 +22,7 @@ class App extends React.Component {
 						<div className="app-main-header">
 							<ul>
 								<li className="main-header-tab">
-									<NavLink to={'/components'} activeClassName="active-main-header-tab">CS UI
-										Components</NavLink>
+									<NavLink to={'/components'} activeClassName="active-main-header-tab">CS UI Components</NavLink>
 								</li>
 								<li className="main-header-tab">
 									<NavLink to={'/icons'} activeClassName="active-main-header-tab">Icons</NavLink>
@@ -35,8 +34,7 @@ class App extends React.Component {
 									<NavLink to={'/accessibility'} activeClassName="active-main-header-tab">Accessibility</NavLink>
 								</li>
 								<li className="main-header-tab">
-									<NavLink to={'/release-notes'} activeClassName="active-main-header-tab">Release
-										Notes</NavLink>
+									<NavLink to={'/release-notes'} activeClassName="active-main-header-tab">Release Notes</NavLink>
 								</li>
 							</ul>
 							<CSImage type="logo" height="2.25rem"/>
@@ -47,6 +45,11 @@ class App extends React.Component {
 							<Route path="/colors" component={CSColorsPreview}/>
 							<Route path="/release-notes" component={ReleaseNotes}/>
 							<Route path="/accessibility" component={Accessibility}/>
+							<Switch>
+								<Redirect exact from="/" to="/components/CSGettingStarted"/>
+								<Redirect exact from="/components" to="/components/CSGettingStarted"/>
+								<Redirect exact from="/icons/" to="/icons/LightningIcons"/>
+							</Switch>
 						</div>
 					</div>
 				</Router>
