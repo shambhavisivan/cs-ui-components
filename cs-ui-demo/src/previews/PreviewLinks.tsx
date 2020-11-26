63ba3459baa351c6644e3e6d649a515b2513d5d5
kseler <karlo.seler@cloudsensesolutions.com>
2020-11-26 15:51:15 +0100

    [cs-ui] Added props filter functionality in props sidebar
    Change-Id: Ia9b5daf94c3408005775e32df58cc40fd185499f

--- cs-ui-demo/src/previews/PreviewLinks.tsx
+++ cs-ui-demo/src/previews/PreviewLinks.tsx
@@ -1,15 +1,45 @@
 import React from 'react';
+import { CSInputSearch } from '@cloudsense/cs-ui-components';
 
 export interface PreviewLinksProps {
 	component?: any;
 }
 
-class PreviewLinks extends React.Component<PreviewLinksProps> {
+export interface PreviewLinksState {
+	term: string;
+}
+
+class PreviewLinks extends React.Component<PreviewLinksProps, PreviewLinksState> {
+
+	constructor(props: PreviewLinksProps) {
+		super(props);
+
+		this.state = {
+			term: ''
+		};
+	}
+
+	handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		this.setState({ term: event.target.value });
+	}
+
+	searchingForProp = (term: any) => {
+		return (example: any) => {
+			return example.propName.toLowerCase().includes(term.toLowerCase()) || false;
+		};
+	}
+
 	render() {
 		return (
 			<>
+				<CSInputSearch
+					label="Search props"
+					labelHidden
+					placeholder="Search props"
+					onChange={this.handleOnChange}
+				/>
 				<div className="prop-sidebar-wrapper">
-					{this.props.component.examples.map((example: any, i: number) => (
+					{this.props.component.examples.filter(this.searchingForProp(this.state.term)).map((example: any, i: number) => (
 						<div className="prop-group" key={i}>
 							<h4>
 								<a href={`#${this.props.component.name}-${example.propName}`} key={example.propName}>
