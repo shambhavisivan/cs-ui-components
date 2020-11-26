8cfd2909f2c4c3228927318a739ce02958fec7e3
kseler <karlo.seler@cloudsensesolutions.com>
2020-11-26 12:58:46 +0100

    [cs-ui] Added version indicator to demo app
    Change-Id: Ib9d9992988da54be81caf1b4c756b1f2db5d7fad

--- cs-ui-demo/src/App.tsx
+++ cs-ui-demo/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.scss';
 import React from 'react';
 
-import {BrowserRouter as Router, Route, NavLink, Redirect, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink, Redirect, Switch } from 'react-router-dom';
 
 import CSComponentsList from './previews/CSComponentsList';
 import CSIconsList from './previews/CSIconsList';
@@ -9,7 +9,7 @@ import CSColorsPreview from './previews/colors/CSColorsPreview';
 import ReleaseNotes from './previews/ReleaseNotes';
 import Accessibility from './previews/Accessibility';
 
-import {CSImage} from '@cloudsense/cs-ui-components';
+import { CSImage, CSChip } from '@cloudsense/cs-ui-components';
 
 class App extends React.Component {
 
@@ -37,18 +37,21 @@ class App extends React.Component {
 									<NavLink to={'/release-notes'} activeClassName="active-main-header-tab">Release Notes</NavLink>
 								</li>
 							</ul>
-							<CSImage type="logo" height="2.25rem"/>
+							<div className="app-main-header-right">
+								<CSChip text="latest published version: 1.0.0-alpha.147" />
+								<CSImage type="logo" height="2.25rem" />
+							</div>
 						</div>
 						<div className="app-body">
-							<Route path="/components" component={CSComponentsList}/>
-							<Route path="/icons" component={CSIconsList}/>
-							<Route path="/colors" component={CSColorsPreview}/>
-							<Route path="/release-notes" component={ReleaseNotes}/>
-							<Route path="/accessibility" component={Accessibility}/>
+							<Route path="/components" component={CSComponentsList} />
+							<Route path="/icons" component={CSIconsList} />
+							<Route path="/colors" component={CSColorsPreview} />
+							<Route path="/release-notes" component={ReleaseNotes} />
+							<Route path="/accessibility" component={Accessibility} />
 							<Switch>
-								<Redirect exact from="/" to="/components/CSGettingStarted"/>
-								<Redirect exact from="/components" to="/components/CSGettingStarted"/>
-								<Redirect exact from="/icons/" to="/icons/LightningIcons"/>
+								<Redirect exact from="/" to="/components/CSGettingStarted" />
+								<Redirect exact from="/components" to="/components/CSGettingStarted" />
+								<Redirect exact from="/icons/" to="/icons/LightningIcons" />
 							</Switch>
 						</div>
 					</div>
