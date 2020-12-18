54509c0bb2324beec913da942a583da3eea22e69
Nikol Badanjak <nikol.badanjak@cloudsensesolutions.com>
2020-12-18 14:11:55 +0100

    [cs-ui] Upgrade components version to alpha.156
    Change-Id: If5270bf5e667f9fa14d840f27cc75fa8b002e71a

--- cs-ui-demo/src/App.tsx
+++ cs-ui-demo/src/App.tsx
@@ -38,7 +38,7 @@ class App extends React.Component {
 								</li>
 							</ul>
 							<div className="app-main-header-right">
-								<CSChip text="latest published version: 1.0.0-alpha.154" />
+								<CSChip text="latest published version: 1.0.0-alpha.156" />
 								<CSImage type="logo" height="2.25rem" />
 							</div>
 						</div>
