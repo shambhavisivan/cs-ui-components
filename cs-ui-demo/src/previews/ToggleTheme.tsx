d9e6d306a38d6a61a0fdac31ed6f15fb1f2c70b0
CLOUDSENSE\dominik.kralj <dominik.kralj@cloudsense.com>
2021-01-26 15:48:46 +0100

    [cs-ui] Demo App - added CSTooltip on dark theme button
    Change-Id: I0cf091baf774b4ac58a9998877308a862c1a5244

--- cs-ui-demo/src/previews/ToggleTheme.tsx
+++ cs-ui-demo/src/previews/ToggleTheme.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
-import {CSButton} from '@cloudsense/cs-ui-components';
+import { CSButton, CSTooltip } from '@cloudsense/cs-ui-components';
 import { useTheme, Theme } from '../context/ThemeContext';
 
 const ToggleTheme: React.FC = () => {
 	const { theme, toggleTheme } = useTheme();
 
-	return <CSButton
-		className="app-toggle-theme"
-		color={theme === Theme.Dark ? 'black' : 'orange'}
-		label="activity icon button"
-		iconName="light_bulb"
-		iconDisplay="icon-only"
-		onClick={toggleTheme}
-	/>;
+	return (
+		<CSTooltip
+			width="10rem"
+			content="WIP: This will be used for dark theme implementation"
+			stylePosition="absolute"
+			position="bottom-center"
+		>
+			<CSButton
+				className="app-toggle-theme"
+				color={theme === Theme.Dark ? 'black' : 'orange'}
+				label="activity icon button"
+				iconName="light_bulb"
+				iconDisplay="icon-only"
+				onClick={toggleTheme}
+			/>
+		</CSTooltip>
+	);
 };
 
 export default ToggleTheme;
