15e048306ed103e13abfb6c31c789f673e31a486
Hari Prathap <hari.prathap@cloudsensesolutions.com>
2021-02-11 10:09:24 +0530

    Run validation only for changed packages
    Change-Id: I7a05de43dbff9dc0516c98298a1e853945b9898f

--- build.sh
+++ build.sh
@@ -2,42 +2,54 @@
 set -e
 export SHELLOPTS
 
-echo "Validating CS UI Components"
-cd cs-ui-components;
-npm ci;
-echo "Running CS UI Components Lint"
-npm run lint;
-echo "Building CS UI Components"
-npm run build;
-echo "Finished validating CS UI Components"
-cd ..;
+changedFiles () {
+	git diff --name-only HEAD~1
+}
 
-echo "Validating CS UI Demo"
-cd cs-ui-demo;
-npm ci;
-echo "Running CS UI Demo Lint"
-npm run lint;
-echo "Finished validating CS UI Demo"
-cd ..;
+if changedFiles | grep -q '^cs-ui-components'; then
+    echo "Validating CS UI Components"
+    cd cs-ui-components;
+    npm ci;
+    echo "Running CS UI Components Lint"
+    npm run lint;
+    echo "Building CS UI Components"
+    npm run build;
+    echo "Finished validating CS UI Components"
+    cd ..;
+fi
 
-echo "Validating cs-form"
-cd cs-form;
-npm ci;
-echo "Running cs-form Tests"
-npm t;
-echo "Building cs-form"
-npm run build;
-echo "Finished validating cs-form"
-cd ..;
+if changedFiles | grep -q '^cs-ui-demo'; then
+    echo "Validating CS UI Demo"
+    cd cs-ui-demo;
+    npm ci;
+    echo "Running CS UI Demo Lint"
+    npm run lint;
+    echo "Finished validating CS UI Demo"
+    cd ..;
+fi
 
-echo "Validating cs-grid"
-cd cs-grid;
-npm ci;
-echo "Running cs-grid Tests"
-npm t;
-echo "Running cs-grid Lint"
-npm run lint;
-echo "Building cs-grid"
-npm run build;
-echo "Finished validating cs-grid"
-cd ..;
+if changedFiles | grep -q '^cs-form'; then
+    echo "Validating cs-form"
+    cd cs-form;
+    npm ci;
+    echo "Running cs-form Tests"
+    npm t;
+    echo "Building cs-form"
+    npm run build;
+    echo "Finished validating cs-form"
+    cd ..;
+fi
+
+if changedFiles | grep -q '^cs-grid'; then
+    echo "Validating cs-grid"
+    cd cs-grid;
+    npm ci;
+    echo "Running cs-grid Tests"
+    npm t;
+    echo "Running cs-grid Lint"
+    npm run lint;
+    echo "Building cs-grid"
+    npm run build;
+    echo "Finished validating cs-grid"
+    cd ..;
+fi
