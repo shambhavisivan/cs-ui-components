a4780fec27aa99932519aee5a0c7a43b8b528d2f
CLOUDSENSE\dominik.kralj <dominik.kralj@cloudsense.com>
2021-02-22 15:34:00 +0100

    [cs-ui] CSLabelPreview - restructured the documentation object
    Change-Id: I76e8a87f7a2bd42dce0d47fe7347dbed3f5e3e99

--- cs-ui-demo/src/previews/components/CSLabelPreview.tsx
+++ cs-ui-demo/src/previews/components/CSLabelPreview.tsx
@@ -1,189 +1,192 @@
 import React from 'react';
-import jsxToString from 'jsx-to-string';
 import PreviewHeading from '../PreviewHeading';
 import PreviewProperties from '../PreviewProperties';
 import PreviewTable from '../PreviewTable';
 import PreviewAccessibility from '../PreviewAccessibility';
 import PreviewLinks from '../PreviewLinks';
 
-import { CSLabel } from '@cloudsense/cs-ui-components';
+import { CSLabel, CSButton } from '@cloudsense/cs-ui-components';
 
 class CSLabelPreview extends React.Component {
-	getDoc() {
-		const json = {
-			name: 'Label',
-			usage: 'This is used to associate value with form field.',
-			accessible: 'yes',
-			examples: [
-				{
-					propName: 'htmlFor',
-					customText: '',
-					variations: [
-						{
-							component:
-								<CSLabel label="Label" htmlFor="Name" />
-						}
-					]
-				},
-				{
-					propName: 'id',
-					variations: [
-						{
-							component:
-								<CSLabel label="Label" id="id" />
-						}
-					]
-				},
-				{
-					propName: 'required',
-					variations: [
-						{
-							variationName: ['true'],
-							quickLink: 'true',
-							component:
-								<CSLabel label="Label" required />
-						}
-					]
-				},
-				{
-					propName: 'helpText',
-					variations: [
-						{
-							component:
-								<CSLabel label="Label" helpText="Help text example" />
-						}
-					]
-				},
-				{
-					propName: 'tooltipPosition',
-					variations: [
-						{
-							variationName: ['top-right'],
-							quickLink: 'top-right',
-							component:
-								<CSLabel label="Label" helpText="Help text example" tooltipPosition="top-right" />
-						}
-					]
-				},
-				{
-					propName: 'title',
-					variations: [
-						{
-							component:
-								<CSLabel label="Label" helpText="Help text example" title="Label" />
-						}
-					]
-				},
-				{
-					propName: 'className',
-					variations: [
-						{
-							variationName: ['custom class'],
-							quickLink: 'custom class',
-							component:
-								<CSLabel label="Label" helpText="Help text example" className="custom-class" />
-						}
-					]
-				}
-			],
-			properties: [
-				{
-					name: 'className',
-					types: ['string'],
-					description: 'Apply custom CSS classes to the label.'
-				}, {
-					name: 'helpText',
-					types: ['string'],
-					description: 'Set the text to be displayed in the tooltip.'
-				}, {
-					name: 'htmlFor',
-					types: ['string'],
-					description: 'Assign the ID of the element the label describes.'
-				}, {
-					name: 'id',
-					types: ['string'],
-					description: 'Set the ID for the label.'
-				}, {
-					name: 'label',
-					required: true,
-					types: ['string'],
-					description: 'Set the label value.'
-				}, {
-					name: 'required',
-					types: ['boolean'],
-					default: 'false',
-					description: 'Set whether the label describes a required field.'
-				}, {
-					name: 'title',
-					types: ['string'],
-					description: 'Set the number input title.'
-				}, {
-					name: 'tooltipPosition',
-					customTypes: [{
-						name: 'CSTooltipPosition',
-						types: [
-							'\'bottom-right\'',
-							'\'bottom-left\'',
-							'\'top-right\'',
-							'\'top-left\'',
-							'\'top-center\'',
-							'\'bottom-center\'',
-							'\'right-top\'',
-							'\'right-center\'',
-							'\'right-bottom\'',
-							'\'left-top\'',
-							'\'left-center\'',
-							'\'left-bottom\''
+	getLabelDoc = () => ({
+		name: 'Label',
+		usage: 'This is used to associate value with form field.',
+		accessible: 'yes',
+		previews: [
+			{
+				name: 'Label',
+				examples: [
+					{
+						propName: 'helpText',
+						variations: [
+							{
+								component: <CSLabel label="Label" helpText="Help text example" />,
+								code: '<CSLabel label="Label" helpText="Help text example" />'
+							}
 						]
-					}],
-					description: 'Set the tooltip position for the label.'
-				}, {
-					name: '[key: string]',
-					types: ['any'],
-					description: 'Spreads the rest of the props to the label tag.'
-				}
-			],
-			accessibility: [
-				{
-					criterionList: [
-						'1.4.1',
-						'1.4.4'
-					],
-					requirements: [
-						{
-							structure: [
-								'HTML `<label>`'
-							]
-						}
-					]
-				}
-			]
-		};
-
-		for (const example of json.examples) {
-			for (const variation of example.variations) {
-				(variation as any).string = jsxToString(variation.component);
+					}, {
+						propName: 'htmlFor',
+						variations: [
+							{
+								component: <>
+									<CSLabel label="Label" htmlFor="Name" />
+									<CSButton label="Button" id="Name" />
+								</>,
+								code: `<>
+									<CSLabel label="Label" htmlFor="Name" />
+									<CSButton label="Button" id="Name" />
+								</>`
+							}
+						]
+					}, {
+						propName: 'required',
+						variations: [
+							{
+								primaryVariants: 'required={true}',
+								component: <CSLabel label="Label" required />,
+								code: '<CSLabel label="Label" required />'
+							}
+						]
+					}, {
+						propName: 'title',
+						variations: [
+							{
+								component: <CSLabel label="Label" title="Title example on a label" />,
+								code: '<CSLabel label="Label" title="Title example on a label" />'
+							}
+						]
+					}, {
+						propName: 'tooltipPosition',
+						alert: {
+							variant: 'info',
+							text: 'This prop can only be used when helpText is set.'
+						},
+						variations: [
+							{
+								primaryVariants: 'tooltipPosition="top-left"',
+								component: <CSLabel
+									label="Label"
+									helpText="Help text example"
+									tooltipPosition="top-left"
+								/>,
+								code: `<CSLabel
+									label="Label"
+									helpText="Help text example"
+									tooltipPosition="top-left"
+								/>`
+							}
+						]
+					}, {
+						propName: 'id | class',
+						variations: [
+							{
+								primaryVariants: [
+									'id="custom-id"',
+									'className="custom-class"'
+								],
+								component: <CSLabel
+									label="Label"
+									id="custom-id"
+									className="custom-class"
+								/>,
+								code: `<CSLabel
+									label="Label"
+									id="custom-id"
+									className="custom-class"
+								/>`
+							}
+						]
+					}
+				]
 			}
-		}
-
-		return json;
-	}
+		],
+		properties: [
+			{
+				name: 'className',
+				types: ['string'],
+				description: 'Apply custom CSS classes to the label.'
+			}, {
+				name: 'helpText',
+				types: ['string'],
+				description: 'Set the text to be displayed in the tooltip.'
+			}, {
+				name: 'htmlFor',
+				types: ['string'],
+				description: 'Assign the ID of the element the label describes.'
+			}, {
+				name: 'id',
+				types: ['string'],
+				description: 'Set the ID for the label.'
+			}, {
+				name: 'label',
+				required: true,
+				types: ['string'],
+				description: 'Set the label value.'
+			}, {
+				name: 'required',
+				types: ['boolean'],
+				default: 'false',
+				description: 'Set whether the label describes a required field.'
+			}, {
+				name: 'title',
+				types: ['string'],
+				description: 'Set the custom title to a label.'
+			}, {
+				name: 'tooltipPosition',
+				customTypes: [{
+					name: 'CSTooltipPosition',
+					types: [
+						'\'bottom-right\'',
+						'\'bottom-left\'',
+						'\'top-right\'',
+						'\'top-left\'',
+						'\'top-center\'',
+						'\'bottom-center\'',
+						'\'right-top\'',
+						'\'right-center\'',
+						'\'right-bottom\'',
+						'\'left-top\'',
+						'\'left-center\'',
+						'\'left-bottom\''
+					]
+				}],
+				default: '\'top-right\'',
+				description: 'Set the tooltip position for the label.'
+			}, {
+				name: '[key: string]',
+				types: ['any'],
+				description: 'Spreads the rest of the props to the label tag.'
+			}
+		],
+		accessibility: [
+			{
+				criterionList: [
+					'1.4.1',
+					'1.4.4'
+				],
+					requirements: [
+					{
+						structure: [
+							'HTML `<label>`'
+						]
+					}
+				]
+			}
+		]
+	})
 
 	render() {
-		const component = this.getDoc();
+		const component = this.getLabelDoc();
 
 		return (
 			<>
 				<div className="preview-section-wrapper">
 					<PreviewHeading name={component.name} usage={component.usage} accessible={component.accessible} />
-					<PreviewProperties name={component.name} examples={component.examples} />
+					<PreviewProperties {...component} />
 					<PreviewTable components={[component]} />
 					<PreviewAccessibility components={[component]} />
 				</div>
-				<div className="prop-sidebar">
-					<h3>Quick Links</h3>
-					<PreviewLinks component={component} />
-				</div>
+				<PreviewLinks {...component} />
 			</>
 		);
 	}
