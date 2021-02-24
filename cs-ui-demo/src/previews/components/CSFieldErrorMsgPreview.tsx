07e9e5685cb947c348eed3fc857079b919a29120
Dario Šehović <dario.sehovic@cloudsense.com>
2021-02-24 10:22:55 +0100

    [cs-ui] CSFieldErrorMsgPreview - restructured the documentation object
    Change-Id: I6562641635223b079453bbf1a5e80a329b32a3e1

--- cs-ui-demo/src/previews/components/CSFieldErrorMsgPreview.tsx
+++ cs-ui-demo/src/previews/components/CSFieldErrorMsgPreview.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import jsxToString from 'jsx-to-string';
 import PreviewHeading from '../PreviewHeading';
 import PreviewProperties from '../PreviewProperties';
 import PreviewTable from '../PreviewTable';
@@ -9,85 +8,88 @@ import PreviewLinks from '../PreviewLinks';
 import { CSFieldErrorMsg } from '@cloudsense/cs-ui-components';
 
 class CSFieldErrorMsgPreview extends React.Component {
-	getDoc() {
-		const json = {
-			name: 'Field Error Message',
-			usage: 'Field Error Message renders error message on form elements.',
-			accessible: 'yes',
-			examples: [
-				{
-					propName: 'message',
-					customText: '',
-					variations: [
-						{
-							variationName: ['string'],
-							quickLink: 'string',
-							component:
-								<CSFieldErrorMsg message="Error message!" />
-						},
-						{
-							variationName: ['Array<string>'],
-							quickLink: 'Array<string>',
-							component:
-								<CSFieldErrorMsg message={['Error message 1!', 'Error message 2!', 'Error message 3!']} />
-						}
-					]
-				}
-			],
-			properties: [
-				{
-					name: 'message',
-					required: true,
-					customTypes: [{
-						name: 'CSFieldErrorMsgType',
-						types: ['string', 'Array<string>']
-					}],
-					description: 'Set the error message text'
-				}, {
-					name: '[key: string]',
-					types: ['any'],
-					description: 'Spreads the rest of the props to the field error message span tag.'
-				}
-			],
-			accessibility: [
-				{
-					criterionList: [
-						'1.4.4'
-					],
-					requirements: [
-						{
-							styling: [
-								'Color contrast ratio > 4.5'
-							]
-						}
-					]
-				}
-			]
-		};
-		for (const example of json.examples) {
-			for (const variation of example.variations) {
-				(variation as any).string = jsxToString(variation.component);
+	getFieldErrorMessageDoc = () => ({
+		name: 'Field Error Message',
+		usage: 'Field Error Message renders error message on form elements.',
+		accessible: 'yes',
+		previews: [
+			{
+				name: 'Field Error Message',
+				examples: [
+					{
+						propName: 'message',
+						variations: [
+							{
+								primaryVariants: 'message="text"',
+								quickLink: 'string',
+								component: <CSFieldErrorMsg message="Error message!" />,
+								code: '<CSFieldErrorMsg message="Error message!" />'
+							}, {
+								primaryVariants: 'message={[...]}',
+								quickLink: 'Array<string>',
+								component: <CSFieldErrorMsg
+									message={[
+										'Error message 1!',
+										'Error message 2!',
+										'Error message 3!'
+									]}
+								/>,
+								code: `<CSFieldErrorMsg
+									message={[
+										'Error message 1!',
+										'Error message 2!',
+										'Error message 3!'
+									]}
+								/>`
+							}
+						]
+					}
+				]
 			}
-		}
-
-		return json;
-	}
+		],
+		properties: [
+			{
+				name: 'message',
+				required: true,
+				customTypes: [{
+					name: 'CSFieldErrorMsgType',
+					types: ['string', 'Array<string>']
+				}],
+				description: 'Set the error message text'
+			}, {
+				name: '[key: string]',
+				types: ['any'],
+				description: 'Spreads the rest of the props to the field error message span tag.'
+			}
+		],
+		accessibility: [
+			{
+				criterionList: [
+					'1.4.4'
+				],
+				requirements: [
+					{
+						styling: [
+							'Color contrast ratio > 4.5'
+						]
+					}
+				]
+			}
+		]
+	})
 
 	render() {
-		const component = this.getDoc();
+		const component = this.getFieldErrorMessageDoc();
 
 		return (
 			<>
 				<div className="preview-section-wrapper divider-preview">
 					<PreviewHeading name={component.name} usage={component.usage} accessible={component.accessible} />
-					<PreviewProperties name={component.name} examples={component.examples} />
+					<PreviewProperties {...component} />
 					<PreviewTable components={[component]} />
 					<PreviewAccessibility components={[component]} />
 				</div>
-				<div className="prop-sidebar">
-					<h3>Quick Links</h3>
-					<PreviewLinks component={component} />
-				</div>
+				<PreviewLinks {...component} />
 			</>
 		);
 	}
