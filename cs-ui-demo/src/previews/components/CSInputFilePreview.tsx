b21024c8938ba41930210b078f0cab014f905c49
Dario Šehović <dario.sehovic@cloudsense.com>
2021-02-24 12:59:40 +0100

    [cs-ui] CSInputFilePreview - restructured the documentation object
    Change-Id: I6e25c49a484e8ffff6ab74774dc3e86b8acbc47f

--- cs-ui-demo/src/previews/components/CSInputFilePreview.tsx
+++ cs-ui-demo/src/previews/components/CSInputFilePreview.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import jsxToString from 'jsx-to-string';
 import PreviewHeading from '../PreviewHeading';
 import PreviewProperties from '../PreviewProperties';
 import PreviewTable from '../PreviewTable';
@@ -11,388 +10,329 @@ import { CSInputFile } from '@cloudsense/cs-ui-components';
 class CSInputFilePreview extends React.Component {
 	handleChange = () => alert('File has been uploaded.');
 	handleDrop = () => alert('File has been dropped.');
-
-	getDoc() {
-		const json = {
-			name: 'Input File',
-			usage: 'Input File component',
-			accessible: 'partially',
-			examples: [
-				{
-					propName: 'label',
-					variations: [
-						{
-							component:
-								<CSInputFile
-									label="Upload directory"
-								/>
-						}
-					]
-				},
-				{
-					propName: 'disabled',
-					variations: [
-						{
-							variationName: ['true'],
-							quickLink: 'true',
-							component:
-								<CSInputFile
-									label="Upload directory"
-									disabled
-								/>
-						}
-					]
-				},
-				{
-					propName: 'accept',
-					variations: [
-						{
-							variationName: ['file extension'],
-							quickLink: 'file extension',
-							component:
-								<CSInputFile
-									label="Upload a file"
-									accept=".png"
-								/>
+	getInputFileDoc = () => ({
+		name: 'Input File',
+		usage: 'Input File component',
+		accessible: 'partially',
+		previews: [
+			{
+				name: 'Input File',
+				examples: [
+					{
+						propName: 'label',
+						alert: {
+							variant: 'info',
+							text: 'Label is a required prop because of accessibility. You need to provide an explanatory label for a file input. If you want to hide the label visually, you can use the labelHidden prop.'
 						},
-						{
-							variationName: ['audio/*'],
-							quickLink: 'audio/*',
-							component:
-								<CSInputFile
-									label="Upload an audio file"
-									accept="audio/*"
-								/>
-						},
-						{
-							variationName: ['video/*'],
-							quickLink: 'video/*',
-							component:
-								<CSInputFile
-									label="Upload a video file"
-									accept="video/*"
-								/>
-						},
-						{
-							variationName: ['image/*'],
-							quickLink: 'image/*',
-							component:
-								<CSInputFile
-									label="Upload an image file"
-									accept="image/*"
-								/>
-						}
-					]
-				},
-				{
-					propName: 'error',
-					alert: {
-						variant: 'info',
-						text: 'Component in error state should always contain associated error message to satisfy accessibility best practices!'
-					},
-					variations: [
-						{
-							variationName: ['true'],
-							quickLink: 'true',
-							component:
-								<CSInputFile
+						variations: [
+							{
+								component: <CSInputFile label="Upload directory" />,
+								code: '<CSInputFile label="Upload directory" />'
+							}
+						]
+					}, {
+						propName: 'accept',
+						variations: [
+							{
+								primaryVariants: 'accept=".png"',
+								quickLink: 'file extension',
+								component: <CSInputFile label="Upload a file" accept=".png" />,
+								code: '<CSInputFile label="Upload a file" accept=".png" />'
+							}, {
+								primaryVariants: 'accept="audio/*"',
+								quickLink: 'audio',
+								component: <CSInputFile label="Upload an audio file" accept="audio/*" />,
+								code: '<CSInputFile label="Upload an audio file" accept="audio/*" />'
+							}, {
+								primaryVariants: 'accept="video/*"',
+								quickLink: 'video',
+								component: <CSInputFile label="Upload a video file" accept="video/*" />,
+								code: '<CSInputFile label="Upload a video file" accept="video/*" />'
+							}, {
+								primaryVariants: 'accept="image/*"',
+								quickLink: 'image',
+								component: <CSInputFile label="Upload an image file" accept="image/*" />,
+								code: '<CSInputFile label="Upload an image file" accept="image/*" />'
+							}
+						]
+					}, {
+						propName: 'disabled',
+						variations: [
+							{
+								primaryVariants: 'disabled={true}',
+								component: <CSInputFile label="Upload directory" disabled />,
+								code: '<CSInputFile label="Upload directory" disabled />'
+							}
+						]
+					}, {
+						propName: 'dropAreaBackground',
+						variations: [
+							{
+								primaryVariants: 'dropAreaBackground={true}',
+								secondaryVariants: [
+									'dropAreaWidth="25rem"',
+									'dropAreaHeight="3rem"'
+								],
+								quickLink: 'true',
+								component: <CSInputFile
 									label="Upload directory"
-									error
-								/>
-						}
-					]
-				},
-				{
-					propName: 'errorMessage',
-					variations: [
-						{
-							variationText: ['error="true"'],
-							component:
-								<CSInputFile
+									dropAreaBackground
+									dropAreaWidth="25rem"
+									dropAreaHeight="3rem"
+								/>,
+								code: `<CSInputFile
+									label="Upload directory"
+									dropAreaBackground
+									dropAreaWidth="25rem"
+									dropAreaHeight="3rem"
+								/>`
+							}, {
+								primaryVariants: 'dropAreaBackground={false}',
+								secondaryVariants: [
+									'dropAreaWidth="25rem"',
+									'dropAreaHeight="3rem"'
+								],
+								quickLink: 'false',
+								component: <CSInputFile
+									label="Upload directory"
+									dropAreaWidth="25rem"
+									dropAreaHeight="3rem"
+								/>,
+								code: `<CSInputFile
+									label="Upload directory"
+									dropAreaWidth="25rem"
+									dropAreaHeight="3rem"
+								/>`
+							}
+						]
+					}, {
+						propName: 'dropAreaHeight',
+						variations: [
+							{
+								primaryVariants: 'dropAreaHeight="100px"',
+								quickLink: '100px',
+								component: <CSInputFile label="Upload directory" dropAreaHeight="100px" />,
+								code: '<CSInputFile label="Upload directory" dropAreaHeight="100px" />'
+							}, {
+								primaryVariants: 'dropAreaHeight="10rem"',
+								quickLink: '10rem',
+								component: <CSInputFile label="Upload directory" dropAreaHeight="10rem" />,
+								code: '<CSInputFile label="Upload directory" dropAreaHeight="10rem" />'
+							}
+						]
+					}, {
+						propName: 'dropAreaWidth',
+						variations: [
+							{
+								primaryVariants: 'dropAreaWidth="100%"',
+								quickLink: '100%',
+								component: <CSInputFile label="Upload directory" dropAreaWidth="100%" />,
+								code: '<CSInputFile label="Upload directory" dropAreaWidth="100%" />'
+							}, {
+								primaryVariants: 'dropAreaWidth="400px"',
+								quickLink: '400px',
+								component: <CSInputFile label="Upload directory" dropAreaWidth="400px" />,
+								code: '<CSInputFile label="Upload directory" dropAreaWidth="400px" />'
+							}
+						]
+					}, {
+						propName: 'error',
+						alert: {
+							variant: 'info',
+							text: 'Component in error state should always contain associated error message to satisfy accessibility best practices!'
+						},
+						variations: [
+							{
+								primaryVariants: 'error={true}',
+								component: <CSInputFile label="Upload directory" error />,
+								code: '<CSInputFile label="Upload directory" error />'
+							}
+						]
+					}, {
+						propName: 'errorMessage',
+						variations: [
+							{
+								secondaryVariants: 'error={true}',
+								component: <CSInputFile
 									label="Upload directory"
 									error
 									errorMessage="File type not supported"
-								/>
-						}
-					]
-				},
-				{
-					propName: 'fileSize',
-					variations: [
-						{
-							variationName: ['true'],
-							quickLink: 'true',
-							string: '',
-							component:
-								<CSInputFile
+								/>,
+								code: `<CSInputFile
 									label="Upload directory"
-									fileSize
-								/>
-						}
-					]
-				},
-				{
-					propName: 'onChange',
-					variations: [
-						{
-							component:
-								<CSInputFile
+									error
+									errorMessage="File type not supported"
+								/>`
+							}
+						]
+					}, {
+						propName: 'fileSize',
+						variations: [
+							{
+								primaryVariants: 'fileSize={true}',
+								component: <CSInputFile label="Upload directory" fileSize />,
+								code: '<CSInputFile label="Upload directory" fileSize />'
+							}
+						]
+					}, {
+						propName: 'onChange',
+						variations: [
+							{
+								component: <CSInputFile label="Upload directory" onChange={this.handleChange} />,
+								code: '<CSInputFile label="Upload directory" onChange={this.handleChange} />'
+							}
+						]
+					}, {
+						propName: 'onDrop',
+						variations: [
+							{
+								component: <CSInputFile label="Upload directory" onDrop={this.handleDrop} />,
+								code: '<CSInputFile label="Upload directory" onDrop={this.handleDrop} />'
+							}
+						]
+					}, {
+						propName: 'id | class',
+						variations: [
+							{
+								primaryVariants: [
+									'id="custom-id"',
+									'className="custom-class"'
+								],
+								component: <CSInputFile
 									label="Upload directory"
-									onChange={this.handleChange}
-								/>
-						}
-					]
-				},
-				{
-					propName: 'onDrop',
-					variations: [
-						{
-							component:
-								<CSInputFile
-									label="Upload directory"
-									onDrop={this.handleDrop}
-								/>
-						}
-					]
-				},
-				{
-					propName: 'dropAreaBackground',
-					variations: [
-						{
-							variationName: ['true'],
-							component:
-								<CSInputFile
-									label="Upload directory"
-									onDrop={this.handleDrop}
-									dropAreaBackground
-									dropAreaWidth="25rem"
-									dropAreaHeight="3rem"
-								/>
-						},
-						{
-							variationName: ['false'],
-							component:
-								<CSInputFile
-									label="Upload directory"
-									onDrop={this.handleDrop}
-									dropAreaWidth="25rem"
-									dropAreaHeight="3rem"
-								/>
-						}
-					]
-				},
-				{
-					propName: 'dropAreaWidth',
-					variations: [
-						{
-							variationName: ['100%'],
-							quickLink: '100%',
-							component:
-								<CSInputFile
-									label="Upload directory"
-									onDrop={this.handleDrop}
-									dropAreaBackground
-									dropAreaWidth="100%"
-								/>
-						},
-						{
-							variationName: ['400px'],
-							quickLink: '400px',
-							component:
-								<CSInputFile
-									label="Upload directory"
-									onDrop={this.handleDrop}
-									dropAreaBackground
-									dropAreaWidth="400px"
-								/>
-						}
-					]
-				},
-				{
-					propName: 'dropAreaHeight',
-					variations: [
-						{
-							variationName: ['100px'],
-							quickLink: '100px',
-							component:
-								<CSInputFile
-									label="Upload directory"
-									onDrop={this.handleDrop}
-									dropAreaBackground
-									dropAreaHeight="100px"
-								/>
-						},
-						{
-							variationName: ['10rem'],
-							quickLink: '10rem',
-							component:
-								<CSInputFile
-									label="Upload directory"
-									onDrop={this.handleDrop}
-									dropAreaBackground
-									dropAreaHeight="10rem"
-								/>
-						}
-					]
-				},
-				{
-					propName: 'id',
-					variations: [
-						{
-							component:
-								<CSInputFile
-									label="Upload directory"
-									id="fileInput"
-								/>
-						}
-					]
-				},
-				{
-					propName: 'className',
-					variations: [
-						{
-							variationName: ['custom class'],
-							quickLink: 'custom class',
-							component:
-								<CSInputFile
-									label="Upload directory"
+									id="custom-id"
 									className="custom-class"
-								/>
-						}
-					]
-				}
-			],
-			properties: [
-				{
-					name: 'accept',
-					types: ['string', 'Array<string>'],
-					description: 'Set which file types should be accepted. (eg. .jpg, .mp3, audio/*, image/*, etc.)'
-				}, {
-					name: 'className',
-					types: ['string'],
-					description: 'Apply custom CSS classes to the file input wrapper.'
-				}, {
-					name: 'disabled',
-					types: ['boolean'],
-					default: 'false',
-					description: 'Disable the file input.'
-				}, {
-					name: 'dropAreaBackground',
-					types: ['boolean'],
-					default: 'false',
-					description: 'Set whether the background area should be gray.'
-				}, {
-					name: 'dropAreaHeight',
-					types: ['string'],
-					description: 'Set the height of the drop area. (eg. 200px, 20rem, etc.)'
-				}, {
-					name: 'dropAreaWidth',
-					types: ['string'],
-					description: 'Set the height of the drop area. (eg. 200px, 20rem, 100%, etc.)'
-				}, {
-					name: 'error',
-					types: ['boolean'],
-					description: 'Toggle the error state.'
-				}, {
-					name: 'errorMessage',
-					customTypes: [{
-						name: 'CSFieldErrorMsgType',
-						types: ['string', 'Array<string>']
-					}],
-					description: 'Set the error message or messages for the file input.'
-				}, {
-					name: 'fileSize',
-					types: ['boolean'],
-					default: 'false',
-					description: 'Display the file size.'
-				}, {
-					name: 'id',
-					types: ['string'],
-					description: 'Set the ID for the file input.'
-				}, {
-					name: 'label',
-					required: true,
-					types: ['string'],
-					description: 'Set the file input label.'
-				}, {
-					name: 'onChange',
-					types: ['(value) => any'],
-					description: 'Handler method for the change event.'
-				}, {
-					name: 'onDrop',
-					types: ['(value) => any'],
-					description: 'Handler method for the drop event.'
-				}, {
-					name: '[key: string]',
-					types: ['any'],
-					description: 'Spreads the rest of the props to the file input.'
-				}
-			],
-			accessibility: [
-				{
-					criterionList: [
-						'1.4.1',
-						'1.4.4',
-						'2.1.1',
-						'2.1.2',
-						'2.4.7',
-						'2.5.3',
-						'3.2.1',
-						'3.2.2',
-						'3.3.1',
-						'3.3.2',
-						'4.1.2'
-					],
-					requirements: [
-						{
-							structure: [
-								'HTML `<input type="file">` - hidden',
-								'`<span>` imitates button with custom style',
-								'`<input>` and `<span>` wrapped in `<label>`'
-							],
-							properties: [
-								'`aria-labelledby` - associate field with label',
-								'`aria-invalid`',
-								'`aria-required`',
-								'`title`'
-							],
-							styling: [
-								'Focus state styles',
-								'`<label>` styled as drop zone area'
-							],
-							keyboardOperability: [
-								'OOTB focusable'
-							]
-						}
-					]
-				}
-			]
-		};
-
-		for (const example of json.examples) {
-			for (const variation of example.variations) {
-				(variation as any).string = jsxToString(variation.component);
+								/>,
+								code: `<CSInputFile
+									label="Upload directory"
+									id="custom-id"
+									className="custom-class"
+								/>`
+							}
+						]
+					}
+				]
 			}
-		}
-
-		return json;
-	}
+		],
+		properties: [
+			{
+				name: 'accept',
+				types: ['string', 'Array<string>'],
+				description: 'Set which file types should be accepted. (eg. .jpg, .mp3, audio/*, image/*, etc.)'
+			}, {
+				name: 'className',
+				types: ['string'],
+				description: 'Apply custom CSS classes to the file input wrapper.'
+			}, {
+				name: 'disabled',
+				types: ['boolean'],
+				default: 'false',
+				description: 'Disable the file input.'
+			}, {
+				name: 'dropAreaBackground',
+				types: ['boolean'],
+				default: 'false',
+				description: 'Set whether the background area should be gray.'
+			}, {
+				name: 'dropAreaHeight',
+				types: ['string'],
+				description: 'Set the height of the drop area. (eg. 200px, 20rem, etc.)'
+			}, {
+				name: 'dropAreaWidth',
+				types: ['string'],
+				description: 'Set the height of the drop area. (eg. 200px, 20rem, 100%, etc.)'
+			}, {
+				name: 'error',
+				types: ['boolean'],
+				description: 'Toggle the error state.'
+			}, {
+				name: 'errorMessage',
+				customTypes: [{
+					name: 'CSFieldErrorMsgType',
+					types: ['string', 'Array<string>']
+				}],
+				description: 'Set the error message or messages for the file input.'
+			}, {
+				name: 'fileSize',
+				types: ['boolean'],
+				default: 'false',
+				description: 'Display the file size.'
+			}, {
+				name: 'id',
+				types: ['string'],
+				description: 'Set the ID for the file input.'
+			}, {
+				name: 'label',
+				required: true,
+				types: ['string'],
+				description: 'Set the file input label.'
+			}, {
+				name: 'onChange',
+				types: ['(value) => any'],
+				description: 'Handler method for the change event.'
+			}, {
+				name: 'onDrop',
+				types: ['(value) => any'],
+				description: 'Handler method for the drop event.'
+			}, {
+				name: '[key: string]',
+				types: ['any'],
+				description: 'Spreads the rest of the props to the file input.'
+			}
+		],
+		accessibility: [
+			{
+				criterionList: [
+					'1.4.1',
+					'1.4.4',
+					'2.1.1',
+					'2.1.2',
+					'2.4.7',
+					'2.5.3',
+					'3.2.1',
+					'3.2.2',
+					'3.3.1',
+					'3.3.2',
+					'4.1.2'
+				],
+				requirements: [
+					{
+						structure: [
+							'HTML `<input type="file">` - hidden',
+							'`<span>` imitates button with custom style',
+							'`<input>` and `<span>` wrapped in `<label>`'
+						],
+						properties: [
+							'`aria-labelledby` - associate field with label',
+							'`aria-invalid`',
+							'`aria-required`',
+							'`title`'
+						],
+						styling: [
+							'Focus state styles',
+							'`<label>` styled as drop zone area'
+						],
+						keyboardOperability: [
+							'OOTB focusable'
+						]
+					}
+				]
+			}
+		]
+	})
 
 	render() {
-		const component = this.getDoc();
+		const component = this.getInputFileDoc();
 
 		return (
 			<>
 				<div className="preview-section-wrapper">
 					<PreviewHeading name={component.name} usage={component.usage} accessible={component.accessible} />
-					<PreviewProperties name={component.name} examples={component.examples} />
+					<PreviewProperties {...component} />
 					<PreviewTable components={[component]} />
 					<PreviewAccessibility components={[component]} />
 				</div>
-				<div className="prop-sidebar">
-					<h3>Quick Links</h3>
-					<PreviewLinks component={component} />
-				</div>
+				<PreviewLinks {...component} />
 			</>
 		);
 	}
