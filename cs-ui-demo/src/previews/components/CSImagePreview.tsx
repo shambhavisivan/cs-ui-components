463c5022c090647f9c2f71b3317ae269f1f83570
Dario Šehović <dario.sehovic@cloudsense.com>
2021-02-24 11:25:33 +0100

    [cs-ui] CSImagePreview - restructured the documentation object
    Change-Id: I9429b339bb398015d28b141ce63f1aa0c417ede8

--- cs-ui-demo/src/previews/components/CSImagePreview.tsx
+++ cs-ui-demo/src/previews/components/CSImagePreview.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import jsxToString from 'jsx-to-string';
 import PreviewHeading from '../PreviewHeading';
 import PreviewProperties from '../PreviewProperties';
 import PreviewTable from '../PreviewTable';
@@ -9,313 +8,271 @@ import PreviewLinks from '../PreviewLinks';
 import { CSImage } from '@cloudsense/cs-ui-components';
 
 class CSImagePreview extends React.Component {
-	getDoc() {
-		const json = {
-			name: 'Image',
-			usage: 'Image is used for rendering logos and logomarks',
-			accessible: 'yes',
-			examples: [
-				{
-					propName: 'type',
-					variations: [
-						{
-							variationName: ['logo'],
-							quickLink: 'logo',
-							component:
-								<CSImage type="logo" />
-						},
-						{
-							variationName: ['logomark'],
-							quickLink: 'logomark',
-							component:
-								<CSImage type="logomark" />
-						}
-					]
-				},
-				{
-					propName: 'color',
-					variations: [
-						{
-							variationName: ['white'],
-							variationText: ['type="logo"', 'longDescription="white cloudsense logo on black background"'],
-							quickLink: 'white logo',
-							component:
-								<div className="black-background">
-									<CSImage
-										type="logo"
-										color="white"
-										variant="initial"
-										longDescription="white cloudsense logo on black background"
-									/>
-								</div>
-						},
-						{
-							variationName: ['purple'],
-							variationText: 'type="logo"',
-							quickLink: 'purple logo',
-							component:
-								<CSImage
-									type="logo"
-									color="purple"
-									variant="initial"
-								/>
-						},
-						{
-							variationName: ['white'],
-							variationText: 'type="logomark"',
-							quickLink: 'white logomark',
-							component:
-								<div className="black-background">
-									<CSImage
-										type="logomark"
-										color="white"
-										variant="initial"
-									/>
-								</div>
-						},
-						{
-							variationName: ['black'],
-							variationText: 'type="logomark"',
-							quickLink: 'black logomark',
-							component:
-								<CSImage
+	getImagePreviewDoc = () => ({
+		name: 'Image',
+		usage: 'Image is used for rendering logos and logomarks',
+		accessible: 'yes',
+		previews: [
+			{
+				name: 'Image',
+				examples: [
+					{
+						propName: 'type',
+						variations: [
+							{
+								primaryVariants: 'type="logo"',
+								quickLink: 'logo',
+								component: <CSImage type="logo" />,
+								code: '<CSImage type="logo" />'
+							}, {
+								primaryVariants: 'type="logomark"',
+								quickLink: 'logomark',
+								component: <CSImage type="logomark" />,
+								code: '<CSImage type="logomark" />'
+							}
+						]
+					}, {
+						propName: 'color',
+						variations: [
+							{
+								primaryVariants: 'color="purple"',
+								secondaryVariants: 'type="logo"',
+								quickLink: 'purple logo',
+								component: <CSImage type="logo" />,
+								code: '<CSImage type="logo" />'
+							}, {
+								primaryVariants: 'color="white"',
+								secondaryVariants: 'type="logo"',
+								quickLink: 'white logo',
+								component: <div className="black-background">
+									<CSImage type="logo" color="white" />
+								</div>,
+								code: `<div className="black-background">
+									<CSImage type="logo" color="white" />
+								</div>`
+							}, {
+								primaryVariants: 'color="purple"',
+								secondaryVariants: 'type="logomark"',
+								quickLink: 'purple logomark',
+								component: <CSImage type="logomark" />,
+								code: '<CSImage type="logomark" />'
+							}, {
+								primaryVariants: 'color="white"',
+								secondaryVariants: 'type="logomark"',
+								quickLink: 'white logomark',
+								component: <div className="black-background">
+									<CSImage type="logomark" color="white" />
+								</div>,
+								code: `<div className="black-background">
+									<CSImage type="logomark" color="white" />
+								</div>`
+							}, {
+								primaryVariants: 'color="black"',
+								secondaryVariants: 'type="logomark"',
+								quickLink: 'black logomark',
+								component: <CSImage type="logomark" color="black" />,
+								code: '<CSImage type="logomark" color="black" />'
+							}
+						]
+					}, {
+						propName: 'height',
+						variations: [
+							{
+								primaryVariants: 'height="15px"',
+								component: <CSImage
 									type="logomark"
-									color="black"
-									variant="initial"
-								/>
-						},
-						{
-							variationName: ['purple'],
-							variationText: 'type="logomark"',
-							quickLink: 'purple logomark',
-							component:
-								<CSImage
-									type="logomark"
-									color="purple"
-									variant="initial"
-								/>
-						}
-					]
-				},
-				{
-					propName: 'variant',
-					variations: [
-						{
-							variationName: ['initial'],
-							variationText: ['type="logo"', 'color="white"'],
-							quickLink: 'initial white logo',
-							component:
-								<div className="black-background">
-									<CSImage
-										type="logo"
-										color="white"
-										variant="initial"
-									/>
-								</div>
-						},
-						{
-							variationName: ['initial'],
-							variationText: ['type="logo"', 'color="purple"'],
-							quickLink: 'initial purple logo',
-							component:
-								<CSImage
-									type="logo"
-									color="purple"
-									variant="initial"
-								/>
-						},
-						{
-							variationName: ['initial'],
-							variationText: ['type="logomark"', 'color="white"'],
-							quickLink: 'initial white logomark',
-							component:
-								<div className="black-background">
-									<CSImage
-										type="logomark"
-										color="white"
-										variant="initial"
-									/>
-								</div>
-						},
-						{
-							variationName: ['reversed'],
-							variationText: ['type="logomark"', 'color="black"'],
-							quickLink: 'reversed black logomark',
-							component:
-								<CSImage
-									type="logomark"
-									color="black"
-									variant="reversed"
-								/>
-						},
-						{
-							variationName: ['reversed'],
-							variationText: ['type="logomark"', 'color="purple"'],
-							quickLink: 'reversed purple logomark',
-							component:
-								<CSImage
-									type="logomark"
-									color="purple"
-									variant="reversed"
-								/>
-						}
-					]
-				},
-				{
-					propName: 'id',
-					variations: [
-						{
-							component:
-								<CSImage
-									type="logomark"
-									color="purple"
-									variant="initial"
-									id="id"
-								/>
-						}
-					]
-				},
-				{
-					propName: 'width',
-					variations: [
-						{
-							variationName: ['30rem'],
-							component:
-								<CSImage
-									type="logomark"
-									color="purple"
-									variant="initial"
-									width="30rem"
-								/>
-						}
-					]
-				},
-				{
-					propName: 'height',
-					variations: [
-						{
-							variationName: ['15px'],
-							component:
-								<CSImage
-									type="logomark"
-									color="black"
-									variant="initial"
 									height="15px"
-								/>
-						}
-					]
-				},
-				{
-					propName: 'className',
-					variations: [
-						{
-							variationName: ['custom class'],
-							quickLink: 'custom class',
-							component:
-								<CSImage
+									color="black"
+								/>,
+								code: `<CSImage
+									type="logomark"
+									height="15px"
+									color="black"
+								/>`
+							}
+						]
+					}, {
+						propName: 'longDescription',
+						variations: [
+							{
+								component: <CSImage type="logo" longDescription="A purple CloudSense logo on a white background." />,
+								code: `<CSImage type="logo" longDescription="A purple CloudSense logo on a white background." />`
+							}
+						]
+					}, {
+						propName: 'variant',
+						alert: {
+							variant: 'info',
+							text: 'The variant prop should only be used with images of type logomark.'
+						},
+						variations: [
+							{
+								primaryVariants: 'variant="initial"',
+								secondaryVariants: [
+									'type="logomark"',
+									'color="purple"'
+								],
+								quickLink: 'initial purple',
+								component: <CSImage type="logomark" />,
+								code: `<CSImage type="logomark" />`
+							}, {
+								primaryVariants: 'variant="reversed"',
+								secondaryVariants: [
+									'type="logomark"',
+									'color="purple"'
+								],
+								quickLink: 'reversed purple',
+								component: <CSImage type="logomark" variant="reversed" />,
+								code: `<CSImage type="logomark" variant="reversed" />`
+							}, {
+								primaryVariants: 'variant="initial"',
+								secondaryVariants: [
+									'type="logomark"',
+									'color="white"'
+								],
+								quickLink: 'initial white',
+								component: <div className="black-background">
+									<CSImage type="logomark" color="white" />
+								</div>,
+								code: `<div className="black-background">
+									<CSImage type="logomark" color="white" />
+								</div>`
+							}, {
+								primaryVariants: 'variant="reversed"',
+								secondaryVariants: [
+									'type="logomark"',
+									'color="black"'
+								],
+								quickLink: 'reversed black',
+								component: <CSImage
 									type="logomark"
 									color="black"
-									variant="initial"
+									variant="reversed"
+								/>,
+								code: `<CSImage
+									type="logomark"
+									color="black"
+									variant="reversed"
+								/>`
+							}
+						]
+					}, {
+						propName: 'width',
+						variations: [
+							{
+								primaryVariants: 'width="8rem"',
+								component: <CSImage type="logomark" width="8rem" />,
+								code: `<CSImage type="logomark" width="8rem" />`
+							}
+						]
+					}, {
+						propName: 'id | class',
+						variations: [
+							{
+								primaryVariants: [
+									'id="custom-id"',
+									'className="custom-class"'
+								],
+								component: <CSImage
+									type="logomark"
+									color="black"
+									id="custom-id"
 									className="custom-class"
-								/>
-						}
-					]
-				}
-			],
-			properties: [
-				{
-					name: 'className',
-					types: ['string'],
-					description: 'Apply custom CSS classes to the image.'
-				}, {
-					name: 'color',
-					customTypes: [{
-						name: 'CSImageColor',
-						types: ['\'white\'', '\'black\'', '\'purple\'']
-					}],
-					default: '\'purple\'',
-					description: 'Set the image color.'
-				}, {
-					name: 'height',
-					types: ['string'],
-					description: 'Set the image height. (eg. 200px, 20rem, 50%, etc.)'
-				}, {
-					name: 'id',
-					types: ['string'],
-					description: 'Set the ID for the image.'
-				}, {
-					name: 'type',
-					required: true,
-					customTypes: [{
-						name: 'CSImageType',
-						types: ['\'logo\'', '\'logomark\'']
-					}],
-					description: 'Set the image type.'
-				}, {
-					name: 'variant',
-					customTypes: [{
-						name: 'CSImageVariant',
-						types: ['\'initial\'', '\'reversed\'']
-					}],
-					default: '\'initial\'',
-					description: 'Set the image variant.'
-				}, {
-					name: 'width',
-					types: ['string'],
-					description: 'Set the image width. (eg. 200px, 20rem, 50%, etc.)'
-				}, {
-					name: 'longDescription',
-					types: ['string'],
-					description: 'Set the expanded description of the image.'
-				}, {
-					name: '[key: string]',
-					types: ['any'],
-					description: 'Spreads the rest of the props to the image tag.'
-				}
-			],
-			accessibility: [
-				{
-					criterionList: [
-						'4.1.2'
-					],
-					requirements: [
-						{
-							structure: [
-								'HTML `<img>`'
-							],
-							properties: [
-								'`alt`'
-							]
-						}
-					]
-				}
-			]
-		};
-
-		for (const example of json.examples) {
-			for (const variation of example.variations) {
-				(variation as any).string = jsxToString(variation.component);
+								/>,
+								code: `<CSImage
+									type="logomark"
+									color="black"
+									id="custom-id"
+									className="custom-class"
+								/>`
+							}
+						]
+					}
+				]
 			}
-		}
-
-		return json;
-	}
+		],
+		properties: [
+			{
+				name: 'className',
+				types: ['string'],
+				description: 'Apply custom CSS classes to the image.'
+			}, {
+				name: 'color',
+				customTypes: [{
+					name: 'CSImageColor',
+					types: ['\'purple\'', '\'white\'', '\'black\'']
+				}],
+				default: '\'purple\'',
+				description: 'Set the image color.'
+			}, {
+				name: 'height',
+				types: ['string'],
+				description: 'Set the image height. (eg. 200px, 20rem, 50%, etc.)'
+			}, {
+				name: 'id',
+				types: ['string'],
+				description: 'Set the ID for the image.'
+			}, {
+				name: 'type',
+				required: true,
+				customTypes: [{
+					name: 'CSImageType',
+					types: ['\'logo\'', '\'logomark\'']
+				}],
+				description: 'Set the image type.'
+			}, {
+				name: 'variant',
+				customTypes: [{
+					name: 'CSImageVariant',
+					types: ['\'initial\'', '\'reversed\'']
+				}],
+				default: '\'initial\'',
+				description: 'Set the image variant.'
+			}, {
+				name: 'width',
+				types: ['string'],
+				description: 'Set the image width. (eg. 200px, 20rem, 50%, etc.)'
+			}, {
+				name: 'longDescription',
+				types: ['string'],
+				description: 'Set the expanded description of the image.'
+			}, {
+				name: '[key: string]',
+				types: ['any'],
+				description: 'Spreads the rest of the props to the image tag.'
+			}
+		],
+		accessibility: [
+			{
+				criterionList: [
+					'4.1.2'
+				],
+				requirements: [
+					{
+						structure: [
+							'HTML `<img>`'
+						],
+						properties: [
+							'`alt`'
+						]
+					}
+				]
+			}
+		]
+	})
 
 	render() {
-		const component = this.getDoc();
+		const component = this.getImagePreviewDoc();
 
 		return (
 			<>
 				<div className="preview-section-wrapper">
 					<PreviewHeading name={component.name} usage={component.usage} accessible={component.accessible} />
-					<PreviewProperties name={component.name} examples={component.examples} />
+					<PreviewProperties {...component} />
 					<PreviewTable components={[component]} />
 					<PreviewAccessibility components={[component]} />
 				</div>
-				<div className="prop-sidebar">
-					<h3>Quick Links</h3>
-					<PreviewLinks component={component} />
-				</div>
+				<PreviewLinks {...component} />
 			</>
 		);
 	}
