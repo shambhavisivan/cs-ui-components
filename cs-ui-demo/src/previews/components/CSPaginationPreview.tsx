aee1ffa5c59a241665425f3a2e71dfa1c2dd651e
Dario Šehović <dario.sehovic@cloudsense.com>
2021-02-24 15:28:25 +0100

    [cs-ui] CSPaginationPreview - restructured the documentation object
    Change-Id: Ib17d6796bb86efdb4041e81cba90843f8a8418b6

--- cs-ui-demo/src/previews/components/CSPaginationPreview.tsx
+++ cs-ui-demo/src/previews/components/CSPaginationPreview.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import jsxToString from 'jsx-to-string';
 import PreviewHeading from '../PreviewHeading';
 import PreviewProperties from '../PreviewProperties';
 import PreviewTable from '../PreviewTable';
@@ -8,48 +7,48 @@ import PreviewLinks from '../PreviewLinks';
 import { CSPaginationWrapper, CSSelect, CSAlert } from '@cloudsense/cs-ui-components';
 
 class CSPaginationPreview extends React.Component {
-	getDoc() {
-		const json = {
-			name: 'Pagination',
-			usage: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-			examples: [
-				{
-					propName: 'Default View',
-					customText: '',
-					variations: [
-						{
-							component:
-								<CSPaginationWrapper>
+	getPaginationDoc = () => ({
+		name: 'Pagination',
+		usage: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		previews: [
+			{
+				name: 'Pagination',
+				examples: [
+					{
+						propName: 'Default View',
+						variations: [
+							{
+								component: <CSPaginationWrapper>
 									<CSSelect label="hidden label" labelHidden>
 										<option>10</option>
 										<option>20</option>
 										<option>50</option>
 									</CSSelect>
-								</CSPaginationWrapper>
-						}
-					]
-				}
-			],
-			properties: [
-				{
-					name: 'n/a',
-					types: ['n/a'],
-					description: 'n/a'
-				}
-			]
-		};
-
-		for (const example of json.examples) {
-			for (const variation of example.variations) {
-				(variation as any).string = jsxToString(variation.component);
+								</CSPaginationWrapper>,
+								code: `<CSPaginationWrapper>
+									<CSSelect label="hidden label" labelHidden>
+										<option>10</option>
+										<option>20</option>
+										<option>50</option>
+									</CSSelect>
+								</CSPaginationWrapper>`
+							}
+						]
+					}
+				]
 			}
-		}
-
-		return json;
-	}
+		],
+		properties: [
+			{
+				name: 'n/a',
+				types: ['n/a'],
+				description: 'n/a'
+			}
+		]
+	})
 
 	render() {
-		const component = this.getDoc();
+		const component = this.getPaginationDoc();
 
 		return (
 			<>
@@ -60,13 +59,10 @@ class CSPaginationPreview extends React.Component {
 						text="This component is under construction and should not be used."
 						styleFormat="scoped"
 					/>
-					<PreviewProperties name={component.name} examples={component.examples} />
+					<PreviewProperties {...component} />
 					<PreviewTable components={[component]} />
 				</div>
-				<div className="prop-sidebar">
-					<h3>Quick Links</h3>
-					<PreviewLinks component={component} />
-				</div>
+				<PreviewLinks {...component} />
 			</>
 		);
 	}
