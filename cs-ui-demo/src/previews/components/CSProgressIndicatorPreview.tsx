ab56f07e05910f3482d0066b7771e59937eccf0c
CLOUDSENSE\dominik.kralj <dominik.kralj@cloudsense.com>
2021-02-25 15:30:23 +0100

    [cs-ui] CSProgressIndicatorPreview -  restructured the documentation object
    Change-Id: I645e455d697696fca7392e2f4a4b9f28913b90fd

--- cs-ui-demo/src/previews/components/CSProgressIndicatorPreview.tsx
+++ cs-ui-demo/src/previews/components/CSProgressIndicatorPreview.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import jsxToString from 'jsx-to-string';
 import PreviewHeading from '../PreviewHeading';
 import PreviewProperties from '../PreviewProperties';
 import PreviewTable from '../PreviewTable';
@@ -10,249 +9,249 @@ import { CSProgressIndicator, CSProgressIndicatorItem } from '@cloudsense/cs-ui-
 
 class CSProgressIndicatorPreview extends React.Component {
 
-	getDoc() {
-		const json = {
-			name: 'Progress Indicator',
-			usage: 'Progress Indicator is a component that communicates to the user the progress of a particular process.',
-			accessible: 'yes',
-			examples: [
-				{
-					propName: 'status',
-					customText: '',
-					variations: [
-						{
-							variationName: ['incomplete'],
-							quickLink: 'incomplete',
-							component:
-								<CSProgressIndicator>
-									<CSProgressIndicatorItem text="Item 1" status="incomplete" />
-									<CSProgressIndicatorItem text="Item 2" status="incomplete" />
-									<CSProgressIndicatorItem text="Item 3" status="incomplete" />
-								</CSProgressIndicator>
-						},
-						{
-							variationName: ['active'],
-							quickLink: 'active',
-							component:
-								<CSProgressIndicator>
+	getProgressIndicatorDoc = () => ({
+		name: 'Progress Indicator',
+		usage: 'Progress Indicator is a component that communicates to the user the progress of a particular process.',
+		accessible: 'yes',
+		previews: [
+			{
+				name: 'Progress Indicator',
+				examples: [
+					{
+						propName: 'id | class',
+						variations: [
+							{
+								primaryVariants: [
+									'id="custom-id"',
+									'className="custom-class"'
+								],
+								component: <CSProgressIndicator id="custom-id" className="custom-class">
+									<CSProgressIndicatorItem text="Item 1" status="complete" />
+									<CSProgressIndicatorItem text="Item 2" status="error" />
+									<CSProgressIndicatorItem text="Item 3" status="complete" />
+									<CSProgressIndicatorItem text="Item 4" status="active" />
+									<CSProgressIndicatorItem text="Item 5" />
+									<CSProgressIndicatorItem text="Item 6" />
+								</CSProgressIndicator>,
+								code: `<CSProgressIndicator id="custom-id" className="custom-class">
+									<CSProgressIndicatorItem text="Item 1" status="complete" />
+									<CSProgressIndicatorItem text="Item 2" status="error" />
+									<CSProgressIndicatorItem text="Item 3" status="complete" />
+									<CSProgressIndicatorItem text="Item 4" status="active" />
+									<CSProgressIndicatorItem text="Item 5" />
+									<CSProgressIndicatorItem text="Item 6" />
+								</CSProgressIndicator>`
+							}
+						]
+					}
+				]
+			}, {
+				name: 'Progress Indicator Item',
+				examples: [
+					{
+						propName: 'text',
+						variations: [
+							{
+								component: <CSProgressIndicator>
+									<CSProgressIndicatorItem text="Item 1" status="complete" />
+									<CSProgressIndicatorItem text="Item 2" status="error" />
+									<CSProgressIndicatorItem text="Item 3" status="active" />
+									<CSProgressIndicatorItem text="Item 4" />
+								</CSProgressIndicator>,
+								code: `<CSProgressIndicator>
+									<CSProgressIndicatorItem text="Item 1" status="complete" />
+									<CSProgressIndicatorItem text="Item 2" status="error" />
+									<CSProgressIndicatorItem text="Item 3" status="active" />
+									<CSProgressIndicatorItem text="Item 4" />
+								</CSProgressIndicator>`
+							}
+						]
+					}, {
+						propName: 'status',
+						variations: [
+							{
+								primaryVariants: 'status="incomplete"',
+								quickLink: 'incomplete',
+								component: <CSProgressIndicator>
+									<CSProgressIndicatorItem text="Item 1" />
+									<CSProgressIndicatorItem text="Item 2" />
+									<CSProgressIndicatorItem text="Item 3"/>
+								</CSProgressIndicator>,
+								code: `<CSProgressIndicator>
+									<CSProgressIndicatorItem text="Item 1" />
+									<CSProgressIndicatorItem text="Item 2" />
+									<CSProgressIndicatorItem text="Item 3" />
+								</CSProgressIndicator>`
+							}, {
+								primaryVariants: 'status="active"',
+								quickLink: 'active',
+								component: <CSProgressIndicator>
 									<CSProgressIndicatorItem text="Item 1" status="active" />
 									<CSProgressIndicatorItem text="Item 2" status="active" />
 									<CSProgressIndicatorItem text="Item 3" status="active" />
-								</CSProgressIndicator>
-						},
-						{
-							variationName: ['complete'],
-							quickLink: 'complete',
-							component:
-								<CSProgressIndicator>
+								</CSProgressIndicator>,
+								code: `<CSProgressIndicator>
+									<CSProgressIndicatorItem text="Item 1" status="active" />
+									<CSProgressIndicatorItem text="Item 2" status="active" />
+									<CSProgressIndicatorItem text="Item 3" status="active" />
+								</CSProgressIndicator>`
+							}, {
+								primaryVariants: 'status="complete"',
+								quickLink: 'complete',
+								component: <CSProgressIndicator>
 									<CSProgressIndicatorItem text="Item 1" status="complete" />
 									<CSProgressIndicatorItem text="Item 2" status="complete" />
 									<CSProgressIndicatorItem text="Item 3" status="complete" />
-								</CSProgressIndicator>
-						},
-						{
-							variationName: ['error'],
-							quickLink: 'error',
-							component:
-								<CSProgressIndicator>
+								</CSProgressIndicator>,
+								code: `<CSProgressIndicator>
+									<CSProgressIndicatorItem text="Item 1" status="complete" />
+									<CSProgressIndicatorItem text="Item 2" status="complete" />
+									<CSProgressIndicatorItem text="Item 3" status="complete" />
+								</CSProgressIndicator>`
+							}, {
+								primaryVariants: 'status="error"',
+								quickLink: 'error',
+								component: <CSProgressIndicator>
 									<CSProgressIndicatorItem text="Item 1" status="error" />
 									<CSProgressIndicatorItem text="Item 2" status="error" />
 									<CSProgressIndicatorItem text="Item 3" status="error" />
-								</CSProgressIndicator>
-						},
-						{
-							variationName: ['loading'],
-							quickLink: 'loading',
-							component:
-								<CSProgressIndicator>
+								</CSProgressIndicator>,
+								code: `<CSProgressIndicator>
+									<CSProgressIndicatorItem text="Item 1" status="error" />
+									<CSProgressIndicatorItem text="Item 2" status="error" />
+									<CSProgressIndicatorItem text="Item 3" status="error" />
+								</CSProgressIndicator>`
+							}, {
+								primaryVariants: 'status="loading"',
+								quickLink: 'loading',
+								component: <CSProgressIndicator>
 									<CSProgressIndicatorItem text="Item 1" status="loading" />
 									<CSProgressIndicatorItem text="Item 2" status="loading" />
 									<CSProgressIndicatorItem text="Item 3" status="loading" />
-								</CSProgressIndicator>
-						}
-					]
-				},
-				{
-					propName: 'text',
-					customText: '',
-					variations: [
-						{
-							component:
-								<CSProgressIndicator>
-									<CSProgressIndicatorItem text="Item 1" status="complete" />
-									<CSProgressIndicatorItem text="Item 2" status="error" />
-									<CSProgressIndicatorItem text="Item 3" status="active" />
-									<CSProgressIndicatorItem text="Item 4" status="incomplete" />
-								</CSProgressIndicator>
-						}
-					]
-				},
-				{
-					propName: 'id',
-					customText: '',
-					variations: [
-						{
-							component:
-								<CSProgressIndicator id="id">
-									<CSProgressIndicatorItem text="Item 1" status="complete" />
-									<CSProgressIndicatorItem text="Item 2" status="error" />
-									<CSProgressIndicatorItem text="Item 3" status="complete" />
-									<CSProgressIndicatorItem text="Item 4" status="active" />
-									<CSProgressIndicatorItem text="Item 5" status="incomplete" />
-									<CSProgressIndicatorItem text="Item 6" status="incomplete" />
-								</CSProgressIndicator>
-						}
-					]
-				},
-				{
-					propName: 'className',
-					customText: '',
-					variations: [
-						{
-							variationName: ['custom class'],
-							quickLink: 'custom class',
-							component:
-								<CSProgressIndicator className="custom-class">
-									<CSProgressIndicatorItem text="Item 1" status="complete" />
-									<CSProgressIndicatorItem text="Item 2" status="error" />
-									<CSProgressIndicatorItem text="Item 3" status="complete" />
-									<CSProgressIndicatorItem text="Item 4" status="active" />
-									<CSProgressIndicatorItem text="Item 5" status="incomplete" />
-									<CSProgressIndicatorItem text="Item 6" status="incomplete" />
-								</CSProgressIndicator>
-						}
-					]
-				}
-			],
-
-			properties: [
-				{
-					name: 'children',
-					customTypes: [{
-						name: 'CSProgressIndicatorChildren',
-						types: ['<CSProgressIndicatorItem />', '\'any\'']
-					}],
-					description: 'This component is designed to support CSProgressIndicatorItem as a child.'
-				}, {
-					name: 'className',
-					types: ['string'],
-					description: 'Apply custom CSS classes to the progress indicator.'
-				}, {
-					name: 'id',
-					types: ['string'],
-					description: 'Set the ID for the progress indicator.'
-				}, {
-					name: '[key: string]',
-					types: ['any'],
-					description: 'Spreads the rest of the props to the progress indicator ordered list tag.'
-				}
-			],
-			accessibility: [
-				{
-					criterionList: [
-						'1.3.1',
-						'1.3.2',
-						'1.4.1',
-						'1.4.3',
-						'2.1.1',
-						'2.1.2',
-						'2.4.3',
-						'3.3.1',
-						'4.1.1',
-						'4.1.2'
-					],
-					requirements: [
-						{
-							structure: [
-								'HTML `<ol>`',
-								'HTML `<li>`',
-								'HTML `<button>`',
-								'Icon as a child of button with attribute `aria-hidden`'
-							],
-							properties: [
-								'`aria-label`',
-								'`title` with status'
-							],
-							styling: [
-								'Color contrast ratio > 4.5',
-								'Distinct hover, active and focus state styles'
-							],
-							keyboardOperability: [
-								'OOTB focusable and supports clicks with enter and space keys'
-							]
-						}
-					]
-				}
-			]
-		};
-
-		for (const example of json.examples) {
-			for (const variation of example.variations) {
-				(variation as any).string = jsxToString(variation.component);
-			}
-		}
-
-		return json;
-	}
-
-	getCSProgressIndicatorItemDoc() {
-		const json = {
-			name: 'Progress Indicator Item',
-			properties: [
-				{
-					name: 'className',
-					types: ['string'],
-					description: 'Apply custom CSS classes to the progress indicator item.'
-				}, {
-					name: 'id',
-					types: ['string'],
-					description: 'Set the ID for the progress indicator item.'
-				}, {
-					name: 'status',
-					customTypes: [{
-						name: 'CSProgressIndicatorItemStatus',
-						types: [
-							'\'incomplete\'',
-							'\'complete\'',
-							'\'active\'',
-							'\'error\'',
-							'\'loading\''
+								</CSProgressIndicator>,
+								code: `<CSProgressIndicator>
+									<CSProgressIndicatorItem text="Item 1" status="loading" />
+									<CSProgressIndicatorItem text="Item 2" status="loading" />
+									<CSProgressIndicatorItem text="Item 3" status="loading" />
+								</CSProgressIndicator>`
+							}
 						]
-					}],
-					default: '\'incomplete\'',
-					description: 'Style the progress indicator item based on the status.'
-				}, {
-					name: 'text',
-					required: true,
-					types: ['string'],
-					description: 'Set the text inside of the progress indicator item.'
-				}, {
-					name: '[key: string]',
-					types: ['any'],
-					description: 'Spreads the rest of the props to the progress indicator item list item tag.'
-				}
-			]
-		};
+					}
+				]
+			}
+		],
+		properties: [
+			{
+				name: 'children',
+				customTypes: [{
+					name: 'CSProgressIndicatorChildren',
+					types: ['<CSProgressIndicatorItem />', 'any']
+				}],
+				description: 'This component is designed to support CSProgressIndicatorItem as a child.'
+			}, {
+				name: 'className',
+				types: ['string'],
+				description: 'Apply custom CSS classes to the progress indicator.'
+			}, {
+				name: 'id',
+				types: ['string'],
+				description: 'Set the ID for the progress indicator.'
+			}, {
+				name: '[key: string]',
+				types: ['any'],
+				description: 'Spreads the rest of the props to the progress indicator ordered list tag.'
+			}
+		],
+		accessibility: [
+			{
+				criterionList: [
+					'1.3.1',
+					'1.3.2',
+					'1.4.1',
+					'1.4.3',
+					'2.1.1',
+					'2.1.2',
+					'2.4.3',
+					'3.3.1',
+					'4.1.1',
+					'4.1.2'
+				],
+				requirements: [
+					{
+						structure: [
+							'HTML `<ol>`',
+							'HTML `<li>`',
+							'HTML `<button>`',
+							'Icon as a child of button with attribute `aria-hidden`'
+						],
+						properties: [
+							'`aria-label`',
+							'`title` with status'
+						],
+						styling: [
+							'Color contrast ratio > 4.5',
+							'Distinct hover, active and focus state styles'
+						],
+						keyboardOperability: [
+							'OOTB focusable and supports clicks with enter and space keys'
+						]
+					}
+				]
+			}
+		]
+	})
 
-		return json;
-	}
+	getProgressIndicatorItemDoc = () => ({
+		name: 'Progress Indicator Item',
+		properties: [
+			{
+				name: 'className',
+				types: ['string'],
+				description: 'Apply custom CSS classes to the progress indicator item.'
+			}, {
+				name: 'id',
+				types: ['string'],
+				description: 'Set the ID for the progress indicator item.'
+			}, {
+				name: 'status',
+				customTypes: [{
+					name: 'CSProgressIndicatorItemStatus',
+					types: [
+						'\'incomplete\'',
+						'\'complete\'',
+						'\'active\'',
+						'\'error\'',
+						'\'loading\''
+					]
+				}],
+				default: '\'incomplete\'',
+				description: 'Style the progress indicator item based on the status.'
+			}, {
+				name: 'text',
+				required: true,
+				types: ['string'],
+				description: 'Set the text inside of the progress indicator item.'
+			}, {
+				name: '[key: string]',
+				types: ['any'],
+				description: 'Spreads the rest of the props to the progress indicator item list item tag.'
+			}
+		]
+	})
 
 	render() {
-		const component = this.getDoc();
-		const component2 = this.getCSProgressIndicatorItemDoc();
+		const component = this.getProgressIndicatorDoc();
+		const component2 = this.getProgressIndicatorItemDoc();
 
 		return (
 			<>
 				<div className="preview-section-wrapper">
 					<PreviewHeading name={component.name} usage={component.usage} accessible={component.accessible} />
-					<PreviewProperties name={component.name} examples={component.examples} />
+					<PreviewProperties {...component} />
 					<PreviewTable components={[component, component2]} />
 					<PreviewAccessibility components={[component]} />
 				</div>
-				<div className="prop-sidebar">
-					<h3>Quick Links</h3>
-					<PreviewLinks component={component} />
-				</div>
+				<PreviewLinks {...component} />
 			</>
 		);
 	}
