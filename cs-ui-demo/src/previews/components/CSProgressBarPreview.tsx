2fcdec6236be7df4c7645eb5a546964ff15e87b2
Dario Šehović <dario.sehovic@cloudsense.com>
2021-02-26 12:08:49 +0100

    [cs-ui] CSProgressBarPreview - restructured the documentation object
    Change-Id: I9d879e331f9803c033f05930817d52ffbf961aae

--- cs-ui-demo/src/previews/components/CSProgressBarPreview.tsx
+++ cs-ui-demo/src/previews/components/CSProgressBarPreview.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import jsxToString from 'jsx-to-string';
 import PreviewHeading from '../PreviewHeading';
 import PreviewProperties from '../PreviewProperties';
 import PreviewTable from '../PreviewTable';
 import PreviewAccessibility from '../PreviewAccessibility';
 import PreviewLinks from '../PreviewLinks';
 
-import { CSProgressBar, CSButton } from '@cloudsense/cs-ui-components';
+import { CSProgressBar, CSButton, CSButtonGroup } from '@cloudsense/cs-ui-components';
 
 export interface CSProgressBarPreviewState {
 	progress: number;
 }
 
 class CSProgressBarPreview extends React.Component<{}, CSProgressBarPreviewState> {
-	state = {
-		progress: 0
-	};
+	state = { progress: 0 };
 
 	restartProgress = () => this.setState({ progress: 0 });
 	renderProgressFull = () => this.setState({ progress: 100 });
@@ -34,360 +31,342 @@ class CSProgressBarPreview extends React.Component<{}, CSProgressBarPreviewState
 		}, 500);
 	}
 
-	getDoc() {
-		const json = {
-			name: 'Progress Bar',
-			usage: 'A progress bar component communicates to the user the progress of a particular process.',
-			accessible: 'yes',
-			examples: [
-				{
-					propName: 'progress',
-					customText: '',
-					variations: [
-						{
-							variationName: ['0%'],
-							quickLink: '0%',
-							component:
-								<CSProgressBar
-									label="Progress"
-									progress="0%"
-								/>
+	getProgressBarDoc = () => ({
+		name: 'Progress Bar',
+		usage: 'A progress bar component communicates to the user the progress of a particular process.',
+		accessible: 'yes',
+		previews: [
+			{
+				name: 'Progress Bar',
+				examples: [
+					{
+						propName: 'label',
+						alert: {
+							variant: 'info',
+							text: 'Label is a required prop because of accessibility. You need to provide an explanatory label for a progress bar. If you want to hide the label visually, you can use the labelHidden prop.'
 						},
-						{
-							variationName: ['50%'],
-							quickLink: '50%',
-							component:
-								<CSProgressBar
+						variations: [
+							{
+								secondaryVariants: 'progress="50%"',
+								component: <CSProgressBar label="Progress" progress="50%" />,
+								code: '<CSProgressBar label="Progress" progress="50%" />'
+							}
+						]
+					}, {
+						propName: 'progress',
+						variations: [
+							{
+								primaryVariants: 'progress="0%"',
+								quickLink: '0%',
+								component: <CSProgressBar label="Progress" progress="0%" />,
+								code: '<CSProgressBar label="Progress" progress="0%" />'
+							}, {
+								primaryVariants: 'progress="50%"',
+								quickLink: '50%',
+								component: <CSProgressBar label="Progress" progress="50%" />,
+								code: '<CSProgressBar label="Progress" progress="50%" />'
+							}, {
+								primaryVariants: 'progress="100%"',
+								quickLink: '100%',
+								component: <CSProgressBar label="Progress" progress="100%" />,
+								code: '<CSProgressBar label="Progress" progress="100%" />'
+							}
+						]
+					}, {
+						propName: 'color',
+						variations: [
+							{
+								primaryVariants: 'color="rgb(74, 38, 171)"',
+								secondaryVariants: 'progress="70%"',
+								quickLink: 'rgb',
+								component: <CSProgressBar
+									label="Progress"
+									progress="70%"
+									color="rgb(74, 38, 171)"
+								/>,
+								code: `<CSProgressBar
+										label="Progress"
+										progress="70%"
+										color="rgb(74, 38, 171)"
+									/>`
+							}, {
+								primaryVariants: 'color="#3cdbc0"',
+								secondaryVariants: 'progress="50%"',
+								quickLink: 'hex',
+								component: <CSProgressBar
 									label="Progress"
 									progress="50%"
-								/>
-						},
-						{
-							variationName: ['100%'],
-							quickLink: '100%',
-							component:
-								<CSProgressBar
+									color="#3cdbc0"
+								/>,
+								code: `<CSProgressBar
 									label="Progress"
-									progress="100%"
-								/>
-						}
-					]
-				},
-				{
-					propName: 'label',
-					customText: '',
-					variations: [
-						{
-							variationText: ['progress="50%"'],
-							component:
-								<CSProgressBar
 									progress="50%"
+									color="#3cdbc0"
+								/>`
+							}
+						]
+					}, {
+						propName: 'labelHidden',
+						variations: [
+							{
+								primaryVariants: 'labelHidden={true}',
+								component: <CSProgressBar
 									label="Progress"
-								/>
-						}
-					]
-				},
-				{
-					propName: 'labelHidden',
-					variations: [
-						{
-							variationName: ['true'],
-							quickLink: 'true',
-							component:
-								<CSProgressBar
 									progress="50%"
-									label="Progress"
 									labelHidden
-								/>
-						}
-					]
-				},
-				{
-					propName: 'labelTitle',
-					variations: [
-						{
-							variationName: ['true'],
-							quickLink: 'true',
-							component:
-								<CSProgressBar
-									progress="50%"
+								/>,
+								code: `<CSProgressBar
 									label="Progress"
+									progress="50%"
+									labelHidden
+								/>`
+							}
+						]
+					}, {
+						propName: 'labelTitle',
+						variations: [
+							{
+								primaryVariants: 'labelTitle={true}',
+								component: <CSProgressBar
+									label="Progress"
+									progress="50%"
 									labelTitle
-								/>
-						}
-					]
-				},
-				{
-					propName: 'id',
-					customText: '',
-					variations: [
-						{
-							component:
-								<CSProgressBar
-									progress="50%"
+								/>,
+								code: `<CSProgressBar
 									label="Progress"
-									id="id"
-								/>
-						}
-					]
-				},
-				{
-					propName: 'progressIndicator',
-					customText: '',
-					variations: [
-						{
-							variationName: ['true'],
-							quickLink: 'true',
-							variationText: ['progress="70%"'],
-							component:
-								<CSProgressBar
+									progress="50%"
+									labelTitle
+								/>`
+							}
+						]
+					}, {
+						propName: 'progressIndicator',
+						variations: [
+							{
+								primaryVariants: 'progressIndicator={true}',
+								secondaryVariants: 'progress="70%"',
+								component: <CSProgressBar
+									label="Progress"
 									progress="70%"
 									progressIndicator
+								/>,
+								code: `<CSProgressBar
 									label="Progress"
-								/>
-						}
-					]
-				},
-				{
-					propName: 'color',
-					customText: '',
-					variations: [
-						{
-							variationName: ['green'],
-							quickLink: 'green',
-							variationText: ['progress="30%"'],
-							component:
-								<CSProgressBar
-									progress="30%"
-									color="green"
-									label="Progress"
-								/>
-						},
-						{
-							variationName: ['#FFAA00'],
-							quickLink: '#FFAA00',
-							variationText: ['progress="50%"'],
-							component:
-								<CSProgressBar
-									progress="50%"
-									color="#FFAA00"
-									label="Progress"
-								/>
-						},
-						{
-							variationName: ['rgb(100,100,255)'],
-							quickLink: 'rgb(100,100,255)',
-							variationText: ['progress="70%"'],
-							component:
-								<CSProgressBar
 									progress="70%"
-									color="rgb(100,100,255)"
+									progressIndicator
+								/>`
+							}
+						]
+					}, {
+						propName: 'thickness',
+						variations: [
+							{
+								primaryVariants: 'thickness="xsmall"',
+								secondaryVariants: 'progress="50%"',
+								quickLink: 'xsmall',
+								component: <CSProgressBar
 									label="Progress"
-								/>
-						}
-					]
-				},
-				{
-					propName: 'thickness',
-					customText: '',
-					variations: [
-						{
-							variationName: ['xsmall'],
-							quickLink: 'xsmall',
-							variationText: ['progress="50%"'],
-							component:
-								<CSProgressBar
 									progress="50%"
 									thickness="xsmall"
+								/>,
+								code: `<CSProgressBar
 									label="Progress"
-								/>
-						},
-						{
-							variationName: ['small'],
-							quickLink: 'small',
-							variationText: ['progress="50%"'],
-							component:
-								<CSProgressBar
+									progress="50%"
+									thickness="xsmall"
+								/>`
+							}, {
+								primaryVariants: 'thickness="small"',
+								secondaryVariants: 'progress="50%"',
+								quickLink: 'small',
+								component: <CSProgressBar
+									label="Progress"
 									progress="50%"
 									thickness="small"
+								/>,
+								code: `<CSProgressBar
 									label="Progress"
-								/>
-						},
-						{
-							variationName: ['medium'],
-							quickLink: 'medium',
-							variationText: ['progress="50%"'],
-							component:
-								<CSProgressBar
 									progress="50%"
-									thickness="medium"
+									thickness="small"
+								/>`
+							}, {
+								primaryVariants: 'thickness="medium"',
+								secondaryVariants: 'progress="50%"',
+								quickLink: 'medium',
+								component: <CSProgressBar label="Progress" progress="50%" />,
+								code: `<CSProgressBar label="Progress" progress="50%" />`
+							}, {
+								primaryVariants: 'thickness="large"',
+								secondaryVariants: 'progress="50%"',
+								quickLink: 'large',
+								component: <CSProgressBar
 									label="Progress"
-								/>
-						},
-						{
-							variationName: ['large'],
-							quickLink: 'large',
-							variationText: ['progress="50%"'],
-							component:
-								<CSProgressBar
 									progress="50%"
 									thickness="large"
+								/>,
+								code: `<CSProgressBar
 									label="Progress"
-								/>
-						}
-					]
-				},
-				{
-					propName: 'title',
-					variations: [
-						{
-							component:
-								<CSProgressBar
 									progress="50%"
-									label="Progress"
-									title="This is a title"
-								/>
-						}
-					]
-				},
-				{
-					propName: 'className',
-					customText: '',
-					variations: [
-						{
-							variationName: ['custom class'],
-							quickLink: 'custom class',
-							variationText: ['progress="50%"'],
-							component:
-								<CSProgressBar
-									progress="50%"
-									label="Progress"
-									className="custom-class"
-								/>
-						},
-						{
-							variationName: ['Simulate progress'],
-							quickLink: 'Simulate progress',
-							component:
-								<CSProgressBar
-									progress={`${this.state.progress}%`}
-									label="Simulate progress"
-								/>
-						}
-					]
-				}
-			],
-
-			properties: [
-				{
-					name: 'className',
-					types: ['string'],
-					description: 'Apply custom CSS classes to the progress bar.'
-				}, {
-					name: 'color',
-					types: ['string'],
-					description: 'Set a custom colour for the progress bar path. (eg. pink, #ff0000, rgba(0, 0, 0, 0.2), etc.)'
-				}, {
-					name: 'id',
-					types: ['string'],
-					description: 'Set the ID for the progress bar.'
-				}, {
-					name: 'label',
-					required: true,
-					types: ['string'],
-					description: 'Set the progress bar label.'
-				}, {
-					name: 'labelHidden',
-					types: ['boolean'],
-					default: 'false',
-					description: 'Hide the progress bar label.'
-				}, {
-					name: 'labelTitle',
-					types: ['boolean'],
-					description: 'Control whether to set the title attribute.'
-				}, {
-					name: 'progress',
-					required: true,
-					types: ['string'],
-					description: 'Set the percentage value of the progress. (eg. 0%, 50%, 100%, etc.)'
-				}, {
-					name: 'progressIndicator',
-					types: ['boolean'],
-					default: 'false',
-					description: 'Show a textual progress indicator.'
-				}, {
-					name: 'thickness',
-					customTypes: [{
-						name: 'CSProgressBarThickness',
-						types: [
-							'\'xsmall\'',
-							'\'small\'',
-							'\'medium\'',
-							'\'large\''
+									thickness="large"
+								/>`
+							}
 						]
-					}],
-					description: 'Set the thickness of the progress bar.'
-				}, {
-					name: 'title',
-					types: ['string'],
-					description: 'Set the title attribute.'
-				}, {
-					name: '[key: string]',
-					types: ['any'],
-					description: 'Spreads the rest of the props to the progress bar wrapper div.'
-				}
-			],
-
-			accessibility: [
-				{
-					criterionList: [
-						'2.5.3',
-						'3.3.1',
-						'4.1.2'
-					],
-					requirements: [
-						{
-							properties: [
-								'`aria-valuenow`',
-								'`aria-valuemin`',
-								'`aria-valuemax`',
-								'`role="progressbar"`'
-							]
-						}
-					]
-				}
-			]
-		};
-
-		for (const example of json.examples) {
-			for (const variation of example.variations) {
-				(variation as any).string = jsxToString(variation.component);
+					}, {
+						propName: 'title',
+						variations: [
+							{
+								component: <CSProgressBar
+									label="Progress"
+									progress="50%"
+									title="This is a title"
+								/>,
+								code: `<CSProgressBar
+									label="Progress"
+									progress="50%"
+									title="This is a title"
+								/>`
+							}
+						]
+					}, {
+						propName: 'id | class',
+						variations: [
+							{
+								primaryVariants: [
+									'id="custom-id"',
+									'className="custom-class"'
+								],
+								secondaryVariants: 'progress="50%"',
+								component: <CSProgressBar
+									label="Progress"
+									progress="50%"
+									id="custom-id"
+									className="custom-class"
+								/>,
+								code: `<CSProgressBar
+									label="Progress"
+									progress="50%"
+									id="custom-id"
+									className="custom-class"
+								/>`
+							}
+						]
+					}, {
+						propName: 'Progress Simulation',
+						variations: [
+							{
+								component: <>
+									<CSProgressBar label="Simulate Progress" progress={`${this.state.progress}%`} />
+									<CSButtonGroup className="simulation-action-buttons">
+										<CSButton label="Reset progress" onClick={this.restartProgress} />
+										<CSButton label="Start delayed progress" onClick={this.renderProgressDelayed} />
+										<CSButton label="Start full progress" onClick={this.renderProgressFull} />
+									</CSButtonGroup>
+								</>,
+								code: `<CSProgressBar label="Simulate progress" progress={` + '${this.state.progress}%' + `} />
+								<CSButtonGroup className="simulation-action-buttons">
+									<CSButton label="Reset progress" onClick={this.restartProgress} />
+									<CSButton label="Start delayed progress" onClick={this.renderProgressDelayed} />
+									<CSButton label="Start full progress" onClick={this.renderProgressFull} />
+								</CSButtonGroup>`
+							}
+						]
+					}
+				]
 			}
-		}
+		],
+		properties: [
+			{
+				name: 'className',
+				types: ['string'],
+				description: 'Apply custom CSS classes to the progress bar.'
+			}, {
+				name: 'color',
+				types: ['string'],
+				description: 'Set a custom colour for the progress bar path. (eg. pink, #ff0000, rgba(0, 0, 0, 0.2), etc.)'
+			}, {
+				name: 'id',
+				types: ['string'],
+				description: 'Set the ID for the progress bar.'
+			}, {
+				name: 'label',
+				required: true,
+				types: ['string'],
+				description: 'Set the progress bar label.'
+			}, {
+				name: 'labelHidden',
+				types: ['boolean'],
+				default: 'false',
+				description: 'Hide the progress bar label.'
+			}, {
+				name: 'labelTitle',
+				types: ['boolean'],
+				description: 'Control whether to set the title attribute.'
+			}, {
+				name: 'progress',
+				required: true,
+				types: ['string'],
+				description: 'Set the percentage value of the progress. (eg. 0%, 50%, 100%, etc.)'
+			}, {
+				name: 'progressIndicator',
+				types: ['boolean'],
+				default: 'false',
+				description: 'Show a textual progress indicator.'
+			}, {
+				name: 'thickness',
+				customTypes: [{
+					name: 'CSProgressBarThickness',
+					types: [
+						'\'xsmall\'',
+						'\'small\'',
+						'\'medium\'',
+						'\'large\''
+					]
+				}],
+				default: '\'medium\'',
+				description: 'Set the thickness of the progress bar.'
+			}, {
+				name: 'title',
+				types: ['string'],
+				description: 'Set the title attribute.'
+			}, {
+				name: '[key: string]',
+				types: ['any'],
+				description: 'Spreads the rest of the props to the progress bar wrapper div.'
+			}
+		],
 
-		return json;
-	}
+		accessibility: [
+			{
+				criterionList: [
+					'2.5.3',
+					'3.3.1',
+					'4.1.2'
+				],
+				requirements: [
+					{
+						properties: [
+							'`aria-valuenow`',
+							'`aria-valuemin`',
+							'`aria-valuemax`',
+							'`role="progressbar"`'
+						]
+					}
+				]
+			}
+		]
+	})
 
 	render() {
-		const component = this.getDoc();
+		const component = this.getProgressBarDoc();
 
 		return (
 			<>
 				<div className="preview-section-wrapper">
 					<PreviewHeading name={component.name} usage={component.usage} accessible={component.accessible} />
-					<PreviewProperties name={component.name} examples={component.examples} />
-					<div className="simulation-action-buttons">
-						<CSButton onClick={this.restartProgress} label="Reset progress" className="simulate-btn" />
-						<CSButton onClick={this.renderProgressDelayed} label="Start delayed progress" className="simulate-btn" />
-						<CSButton onClick={this.renderProgressFull} label="Start full progress" className="simulate-btn" />
-					</div>
+					<PreviewProperties {...component} />
 					<PreviewTable components={[component]} />
 					<PreviewAccessibility components={[component]} />
 				</div>
-				<div className="prop-sidebar">
-					<h3>Quick Links</h3>
-					<PreviewLinks component={component} />
-				</div>
+				<PreviewLinks {...component} />
 			</>
 		);
 	}
