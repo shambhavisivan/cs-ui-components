6a84b4ef30d3a33859afcfdae4995a00c0f844b7
Dario Šehović <dario.sehovic@cloudsense.com>
2021-03-24 10:41:47 +0100

    [cs-ui] CSSectionPreview - restructured the documentation object
    Change-Id: I7d9dd3ee4a5805b6cf0a2056033348e3855be385

--- cs-ui-demo/src/previews/components/CSSectionPreview.tsx
+++ cs-ui-demo/src/previews/components/CSSectionPreview.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import jsxToString from 'jsx-to-string';
 import PreviewHeading from '../PreviewHeading';
 import PreviewProperties from '../PreviewProperties';
 import PreviewTable from '../PreviewTable';
@@ -9,187 +8,215 @@ import PreviewLinks from '../PreviewLinks';
 import { CSButton, CSSection } from '@cloudsense/cs-ui-components';
 
 class CSSectionPreview extends React.Component {
-	getDoc() {
-		const json = {
-			name: 'Section',
-			usage: 'This is used as a toggle visibility of section content.',
-			accessible: 'yes',
-			examples: [
-				{
-					propName: 'collapsible',
-					customText: '',
-					variations: [
-						{
-							variationName: ['true'],
-							quickLink: 'true',
-							component:
-								<CSSection collapsible title="Collapsible Section">
+	getSectionDoc = () => ({
+		name: 'Section',
+		usage: 'This is used as a toggle visibility of section content.',
+		accessible: 'yes',
+		previews: [
+			{
+				name: 'Section',
+				examples: [
+					{
+						propName: 'title',
+						variations: [
+							{
+								component: <CSSection title="Section Title">
 									<ul>
 										<li>Item 1</li>
 										<li>Item 2</li>
 										<li>Item 3</li>
 									</ul>
-								</CSSection>
-						}
-					]
-				},
-				{
-					propName: 'id',
-					variations: [
-						{
-							component:
-								<CSSection
-									title="Section is closed by default"
-									id="id"
+								</CSSection>,
+								code: `<CSSection title="Section Title">
+									<ul>
+										<li>Item 1</li>
+										<li>Item 2</li>
+										<li>Item 3</li>
+									</ul>
+								</CSSection>`
+							}
+						]
+					}, {
+						propName: 'collapsible',
+						variations: [
+							{
+								primaryVariants: 'collapsible={true}',
+								component: <CSSection title="Section Title" collapsible>
+									<ul>
+										<li>Item 1</li>
+										<li>Item 2</li>
+										<li>Item 3</li>
+									</ul>
+								</CSSection>,
+								code: `<CSSection title="Section Title" collapsible>
+									<ul>
+										<li>Item 1</li>
+										<li>Item 2</li>
+										<li>Item 3</li>
+									</ul>
+								</CSSection>`
+							}
+						]
+					}, {
+						propName: 'defaultClosed',
+						alert: {
+							variant: 'info',
+							text: 'This prop should only be used when the collapsible prop is set to true.'
+						},
+						variations: [
+							{
+								primaryVariants: 'defaultClosed={true}',
+								secondaryVariants: 'collapsible={true}',
+								component: <CSSection
+									title="Section Title"
+									defaultClosed
+									collapsible
 								>
 									<ul>
 										<li>Item 1</li>
 										<li>Item 2</li>
 										<li>Item 3</li>
 									</ul>
-								</CSSection>
-						}
-					]
-				},
-				{
-					propName: 'defaultClosed',
-					variations: [
-						{
-							variationName: ['true'],
-							quickLink: 'true',
-							component:
-								<CSSection defaultClosed collapsible title="Section is closed by default">
+								</CSSection>,
+								code: `<CSSection
+									title="Section Title"
+									defaultClosed
+									collapsible
+								>
 									<ul>
 										<li>Item 1</li>
 										<li>Item 2</li>
 										<li>Item 3</li>
 									</ul>
-								</CSSection>
-						}
-					]
-				},
-				{
-					propName: 'className',
-					variations: [
-						{
-							variationName: ['custom class'],
-							quickLink: 'custom class',
-							variationText: ['collapsible="true"'],
-							component:
-								<CSSection collapsible title="Collapsible Section" className="custom-class">
+								</CSSection>`
+							}
+						]
+					}, {
+						propName: 'id | class',
+						variations: [
+							{
+								primaryVariants: [
+									'id="custom-id"',
+									'className="custom-class"'
+								],
+								component: <CSSection
+									title="Section Title"
+									id="custom-id"
+									className="custom-class"
+								>
 									<ul>
 										<li>Item 1</li>
 										<li>Item 2</li>
 										<li>Item 3</li>
 									</ul>
-								</CSSection>
-						}
-					]
-				},
-				{
-					propName: 'children',
-					customText: 'CSSection supports custom content provided as a child',
-					variations: [
-						{
-							variationName: ['custom component'],
-							quickLink: 'custom component',
-							variationText: ['collapsible="true"'],
-							component:
-								<CSSection collapsible title="Collapsible Section" className="custom-class">
-									<CSButton label="Custom Button" />
-								</CSSection>
-						}
-					]
-				}
-			],
-			properties: [
-				{
-					name: 'children',
-					types: ['any'],
-					description: 'This component supports custom content passed as children.'
-				}, {
-					name: 'className',
-					types: ['string'],
-					description: 'Apply custom CSS classes to the section.'
-				},  {
-					name: 'collapsible',
-					types: ['boolean'],
-					default: 'false',
-					description: 'Control whether the section should be collapsible.'
-				}, {
-					name: 'defaultClosed',
-					types: ['boolean'],
-					default: 'false',
-					description: 'Control whether the section is closed by default. It is designed to be used with collapsible prop.'
-				}, {
-					name: 'id',
-					types: ['string'],
-					description: 'Set the ID for the section.'
-				}, {
-					name: 'title',
-					required: true,
-					types: ['string'],
-					description: 'Set a title for the section.'
-				}, {
-					name: '[key: string]',
-					types: ['any'],
-					description: 'Spreads the rest of the props to the section tag.'
-				}
-			],
-			accessibility: [
-				{
-					criterionList: [
-						'1.4.4',
-						'2.1.1',
-						'2.1.2',
-						'4.1.2'
-					],
-					requirements: [
-						{
-							structure: [
-								'`<section>` is top wrapper - implicit `role="region"`',
-								'Heading is `<h3>` - allows screen reader heading search',
-								'Child is `<button>`'
-							],
-							properties: [
-								'`aria-expanded` - reads out whether section is expanded or collapsed, if expandable'
-							],
-							styling: [
-								'Focus state styles'
-							],
-							keyboardOperability: [
-								'Child is `<button>` - allows focus and supports clicks with `enter` and `space` keys'
-							]
-						}
-					]
-				}
-			]
-		};
-
-		for (const example of json.examples) {
-			for (const variation of example.variations) {
-				(variation as any).string = jsxToString(variation.component);
+								</CSSection>,
+								code: `<CSSection
+									title="Section Title"
+									id="custom-id"
+									className="custom-class"
+								>
+									<ul>
+										<li>Item 1</li>
+										<li>Item 2</li>
+										<li>Item 3</li>
+									</ul>
+								</CSSection>`
+							}
+						]
+					}, {
+						propName: 'children',
+						description: 'CSSection supports custom content provided as a child.',
+						variations: [
+							{
+								primaryVariants: 'custom component',
+								component: <CSSection title="Section Title">
+									<CSButton label="Custom Child Button" />
+								</CSSection>,
+								code: `<CSSection title="Section Title">
+									<CSButton label="Custom Child Button" />
+								</CSSection>`
+							}
+						]
+					}
+				]
 			}
-		}
-
-		return json;
-	}
+		],
+		properties: [
+			{
+				name: 'children',
+				types: ['any'],
+				description: 'This component supports custom content passed as children.'
+			}, {
+				name: 'className',
+				types: ['string'],
+				description: 'Apply custom CSS classes to the section.'
+			}, {
+				name: 'collapsible',
+				types: ['boolean'],
+				default: 'false',
+				description: 'Control whether the section should be collapsible.'
+			}, {
+				name: 'defaultClosed',
+				types: ['boolean'],
+				default: 'false',
+				description: 'Control whether the section is closed by default. It is designed to be used with collapsible prop.'
+			}, {
+				name: 'id',
+				types: ['string'],
+				description: 'Set the ID for the section.'
+			}, {
+				name: 'title',
+				required: true,
+				types: ['string'],
+				description: 'Set a title for the section.'
+			}, {
+				name: '[key: string]',
+				types: ['any'],
+				description: 'Spreads the rest of the props to the section tag.'
+			}
+		],
+		accessibility: [
+			{
+				criterionList: [
+					'1.4.4',
+					'2.1.1',
+					'2.1.2',
+					'4.1.2'
+				],
+				requirements: [
+					{
+						structure: [
+							'`<section>` is top wrapper - implicit `role="region"`',
+							'Heading is `<h3>` - allows screen reader heading search',
+							'Child is `<button>`'
+						],
+						properties: [
+							'`aria-expanded` - reads out whether section is expanded or collapsed, if expandable'
+						],
+						styling: [
+							'Focus state styles'
+						],
+						keyboardOperability: [
+							'Child is `<button>` - allows focus and supports clicks with `enter` and `space` keys'
+						]
+					}
+				]
+			}
+		]
+	})
 
 	render() {
-		const component = this.getDoc();
+		const component = this.getSectionDoc();
 
 		return (
 			<>
 				<div className="preview-section-wrapper">
 					<PreviewHeading name={component.name} usage={component.usage} accessible={component.accessible} />
-					<PreviewProperties name={component.name} examples={component.examples} />
+					<PreviewProperties {...component} />
 					<PreviewTable components={[component]} />
 					<PreviewAccessibility components={[component]} />
 				</div>
-				<div className="prop-sidebar">
-					<h3>Quick Links</h3>
-					<PreviewLinks component={component} />
-				</div>
+				<PreviewLinks {...component} />
 			</>
 		);
 	}
