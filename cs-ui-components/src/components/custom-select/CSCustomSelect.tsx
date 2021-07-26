7c473e2cd4646058a8facddc26cdb55de94898cb
Dario Šehović <dario.sehovic@cloudsense.com>
2021-07-26 12:51:42 +0200

    [cs-ui] eslint - fixed no-case-declaration rule
    Change-Id: I76f27c5023882a1c24f237d8549b80ca467a5ed4

--- cs-ui-components/src/components/custom-select/CSCustomSelect.tsx
+++ cs-ui-components/src/components/custom-select/CSCustomSelect.tsx
@@ -186,8 +186,7 @@ class CSCustomSelect extends React.Component<CSCustomSelectProps, CSCustomSelect
 				this.setState({ activeListItem: 0 });
 				break;
 			default:
-				const _activeListItem = activeListItem + 1;
-				this.setState({ activeListItem: _activeListItem });
+				this.setState({ activeListItem: activeListItem + 1 });
 				break;
 			}
 			break;
@@ -201,8 +200,7 @@ class CSCustomSelect extends React.Component<CSCustomSelectProps, CSCustomSelect
 				this.setState({ activeListItem: lastListElement });
 				break;
 			default:
-				const _activeListItem = activeListItem - 1;
-				this.setState({ activeListItem: _activeListItem });
+				this.setState({ activeListItem: activeListItem - 1 });
 				break;
 			}
 			break;
