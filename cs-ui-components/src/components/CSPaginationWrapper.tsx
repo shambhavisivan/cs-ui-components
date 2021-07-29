72387dde7ddfdd9526c78c30aedd17020bf8343f
Dario Šehović <dario.sehovic@cloudsense.com>
2021-07-29 15:07:24 +0200

    [cs-ui] eslint - fixed no-lone-blocks rule
    Change-Id: Ib7edc1261b8cd24b7d52675671fd7700c1787627

--- cs-ui-components/src/components/CSPaginationWrapper.tsx
+++ cs-ui-components/src/components/CSPaginationWrapper.tsx
@@ -5,45 +5,27 @@ import CSPagination from './CSPagination';
 export interface CSPaginationWrapperProps {
 	className?: string;
 	id?: string;
-	props?: any;
+	children?: any;
 }
 
-class CSPaginationWrapper extends React.Component<CSPaginationWrapperProps> {
-	constructor(props: CSPaginationWrapperProps) {
-		super(props);
+const CSPaginationWrapper = ({
+	children,
+	className,
+	...rest
+}: CSPaginationWrapperProps) => {
+	const paginationClasses = classNames(
+		'cs-pagination-wrapper',
+		{
+			[`${className}`]: className,
+		},
+	);
 
-		// an example array of 150 items to be paged
-		{ /* var exampleItems = [...Array(150).keys()].map(i => ({ id: (i+1), name: 'Item ' + (i+1) }));
-
-		this.state = {
-			exampleItems: exampleItems,
-			pageOfItems: []
-		};
-
-		// bind function in constructor instead of render (https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-bind.md)
-		this.onChangePage = this.onChangePage.bind(this); */ }
-	}
-
-	// onChangePage(pageOfItems) {
-	// // update state with new page of items
-	// this.setState({ pageOfItems: pageOfItems });
-	// }
-
-	render() {
-		const paginationClasses = classNames(
-			'cs-pagination-wrapper',
-			{
-				[`${this.props.className}`]: this.props.className,
-			},
-		);
-		return (
-			<div className={paginationClasses}>
-				<CSPagination />
-				{this.props.children}
-			</div>
-
-		);
-	}
-}
+	return (
+		<div className={paginationClasses} {...rest}>
+			<CSPagination />
+			{children}
+		</div>
+	);
+};
 
 export default CSPaginationWrapper;
