e2521657f04cef39a9ad443fa3bd00716eff09b7
kseler <karlo.seler@cloudsensesolutions.com>
2021-10-26 12:13:52 +0200

    [cs-form-v2] CSFormLookupField - added tests
    Change-Id: I4b5db80cfff5ee0d299c9c9b13df056d622f3f13

---
+++ cs-form-v2/src/__tests__/form-fields/CSFormLookupField.spec.tsx
@@ -0,0 +1,444 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import '../../setupTests';
+import CSFormLookupField from '../../form-fields/CSFormLookupField';
+
+const label = 'label';
+const fieldType = 'LOOKUP';
+const name = 'lookup';
+const modeClient = 'client';
+const modeServer = 'server';
+const fieldToBeDisplayed = 'Account';
+const fetchOptions: any = jest.fn();
+const pageSize = 5;
+const columns = [
+	{ key: 'Account', header: 'Account' },
+	{ key: 'Industry', header: 'Industry' },
+];
+const lookupOptions = [
+	{ key: 1, data: { Id: 1, Account: 'Acme', Industry: 'Manufacturing' } },
+	{ key: 2, data: { Id: 2, Account: 'Global Media', Industry: 'Industry' } },
+	{ key: 3, data: { Id: 3, Account: 'Salesforce', Industry: 'Software' } },
+];
+const actions = [
+	{
+		action: () => jest.fn(),
+		icon: { iconName: 'check' },
+		name: 'Check',
+	},
+	{
+		action: () => jest.fn(),
+		icon: { iconName: 'add' },
+		name: 'Add',
+	},
+];
+const icons = [{ iconName: 'activity' }];
+
+describe('CSFormLookupField', () => {
+	// Common props
+	it('should pass correct mode to CSLookup', () => {
+		const uut = shallow(
+			<CSFormLookupField
+				fieldToBeDisplayed={fieldToBeDisplayed}
+				fieldType={fieldType}
+				label={label}
+				name={name}
+				mode={modeClient}
+				columns={columns}
+				options={lookupOptions}
+			/>,
+		);
+		const lookup = uut.find('CSLookup');
+		expect(lookup.prop('mode')).toBe(modeClient);
+	});
+
+	it('should pass correct label to CSLookup', () => {
+		const uut = shallow(
+			<CSFormLookupField
+				fieldToBeDisplayed={fieldToBeDisplayed}
+				fieldType={fieldType}
+				label={label}
+				name={name}
+				mode={modeClient}
+				columns={columns}
+				options={lookupOptions}
+			/>,
+		);
+		const lookup = uut.find('CSLookup');
+		expect(lookup.prop('label')).toBe(label);
+	});
+
+	it('should pass correct name to CSLookup', () => {
+		const uut = shallow(
+			<CSFormLookupField
+				fieldToBeDisplayed={fieldToBeDisplayed}
+				fieldType={fieldType}
+				label={label}
+				name={name}
+				mode={modeClient}
+				columns={columns}
+				options={lookupOptions}
+			/>,
+		);
+		const lookup = uut.find('CSLookup');
+		expect(lookup.prop('name')).toBe(name);
+	});
+
+	it('should pass correct actions list to CSLookup', () => {
+		const uut = shallow(
+			<CSFormLookupField
+				fieldToBeDisplayed={fieldToBeDisplayed}
+				fieldType={fieldType}
+				label={label}
+				name={name}
+				mode={modeClient}
+				columns={columns}
+				options={lookupOptions}
+				actions={actions}
+			/>,
+		);
+		const lookup = uut.find('CSLookup');
+		expect(JSON.stringify(lookup.prop('actions'))).toBe(JSON.stringify(actions));
+	});
+
+	it('should pass correct disabled value to CSLookup', () => {
+		const uut = shallow(
+			<CSFormLookupField
+				fieldToBeDisplayed={fieldToBeDisplayed}
+				fieldType={fieldType}
+				label={label}
+				name={name}
+				mode={modeClient}
+				columns={columns}
+				options={lookupOptions}
+				disabled
+			/>,
+		);
+		const lookup = uut.find('CSLookup');
+		expect(lookup.prop('disabled')).toBeTruthy();
+	});
+
+	it('should pass correct error value to CSLookup', () => {
+		const uut = shallow(
+			<CSFormLookupField
+				fieldToBeDisplayed={fieldToBeDisplayed}
+				fieldType={fieldType}
+				label={label}
+				name={name}
+				mode={modeClient}
+				columns={columns}
+				options={lookupOptions}
+				error
+			/>,
+		);
+		const lookup = uut.find('CSLookup');
+		expect(lookup.prop('error')).toBeTruthy();
+	});
+
+	it('should pass correct error message to CSLookup', () => {
+		const errorMessage = 'Error message.';
+		const uut = shallow(
+			<CSFormLookupField
+				fieldToBeDisplayed={fieldToBeDisplayed}
+				fieldType={fieldType}
+				label={label}
+				name={name}
+				mode={modeClient}
+				columns={columns}
+				options={lookupOptions}
+				error
+				errorMessage={errorMessage}
+			/>,
+		);
+		const lookup = uut.find('CSLookup');
+		expect(lookup.prop('errorMessage')).toBe(errorMessage);
+	});
+
+	it('should pass correct help text to CSLookup', () => {
+		const helpText = 'Help text.';
+		const uut = shallow(
+			<CSFormLookupField
+				fieldToBeDisplayed={fieldToBeDisplayed}
+				fieldType={fieldType}
+				label={label}
+				name={name}
+				mode={modeClient}
+				columns={columns}
+				options={lookupOptions}
+				helpText={helpText}
+			/>,
+		);
+		const lookup = uut.find('CSLookup');
+		expect(lookup.prop('helpText')).toBe(helpText);
+	});
+
+	it('should pass correct icons list to CSLookup', () => {
+		const uut = shallow(
+			<CSFormLookupField
+				fieldToBeDisplayed={fieldToBeDisplayed}
+				fieldType={fieldType}
+				label={label}
+				name={name}
+				mode={modeClient}
+				columns={columns}
+				options={lookupOptions}
+				icons={icons}
+			/>,
+		);
+		const lookup = uut.find('CSLookup');
+		expect(JSON.stringify(lookup.prop('icons'))).toBe(JSON.stringify(icons));
+	});
+
+	it('should call onBlur event', () => {
+		const handleOnBlurMock = jest.fn();
+		const uut = shallow(
+			<CSFormLookupField
+				fieldToBeDisplayed={fieldToBeDisplayed}
+				fieldType={fieldType}
+				label={label}
+				name={name}
+				mode={modeClient}
+				columns={columns}
+				options={lookupOptions}
+				onBlur={handleOnBlurMock}
+			/>,
+		);
+
+		const lookup = uut.find('CSLookup');
+		(lookup.props() as any).onBlur();
+		expect(handleOnBlurMock).toHaveBeenCalled();
+	});
+
+	it('should call onChange event', () => {
+		const handleOnChangeMock = jest.fn();
+		const uut = shallow(
+			<CSFormLookupField
+				fieldToBeDisplayed={fieldToBeDisplayed}
+				fieldType={fieldType}
+				label={label}
+				name={name}
+				mode={modeClient}
+				columns={columns}
+				options={lookupOptions}
+				onChange={handleOnChangeMock}
+			/>,
+		);
+
+		const lookup = uut.find('CSLookup');
+		(lookup.props() as any).onSelectChange();
+		expect(handleOnChangeMock).toHaveBeenCalled();
+	});
+
+	it('should pass correct readOnly value to CSLookup', () => {
+		const uut = shallow(
+			<CSFormLookupField
+				fieldToBeDisplayed={fieldToBeDisplayed}
+				fieldType={fieldType}
+				label={label}
+				name={name}
+				mode={modeClient}
+				columns={columns}
+				options={lookupOptions}
+				readOnly
+			/>,
+		);
+		const lookup = uut.find('CSLookup');
+		expect(lookup.prop('readOnly')).toBeTruthy();
+	});
+
+	it('should pass correct required value to CSLookup', () => {
+		const uut = shallow(
+			<CSFormLookupField
+				fieldToBeDisplayed={fieldToBeDisplayed}
+				fieldType={fieldType}
+				label={label}
+				name={name}
+				mode={modeClient}
+				columns={columns}
+				options={lookupOptions}
+				required
+			/>,
+		);
+		const lookup = uut.find('CSLookup');
+		expect(lookup.prop('required')).toBeTruthy();
+	});
+
+	it('should pass correct styleClass to CSLookup', () => {
+		const styleClass = 'custom-class';
+		const uut = shallow(
+			<CSFormLookupField
+				fieldToBeDisplayed={fieldToBeDisplayed}
+				fieldType={fieldType}
+				label={label}
+				name={name}
+				mode={modeClient}
+				columns={columns}
+				options={lookupOptions}
+				styleClass={styleClass}
+			/>,
+		);
+		const lookup = uut.find('CSLookup');
+		expect(lookup.prop('className')).toBe(styleClass);
+	});
+
+	it('should pass correct title to CSLookup', () => {
+		const title = 'title';
+		const uut = shallow(
+			<CSFormLookupField
+				fieldToBeDisplayed={fieldToBeDisplayed}
+				fieldType={fieldType}
+				label={label}
+				name={name}
+				mode={modeClient}
+				columns={columns}
+				options={lookupOptions}
+				title={title}
+			/>,
+		);
+		const lookup = uut.find('CSLookup');
+		expect(lookup.prop('title')).toBe(title);
+	});
+
+	it('should pass correct value to CSLookup', () => {
+		const value = { key: 1, data: { Id: 1, Account: 'Acme', Industry: 'Manufacturing' } };
+		const uut = shallow(
+			<CSFormLookupField
+				fieldToBeDisplayed={fieldToBeDisplayed}
+				fieldType={fieldType}
+				label={label}
+				name={name}
+				mode={modeClient}
+				columns={columns}
+				options={lookupOptions}
+				value={value}
+			/>,
+		);
+		const lookup = uut.find('CSLookup');
+		expect(JSON.stringify(lookup.prop('value'))).toBe(JSON.stringify(value));
+	});
+
+	// Client mode props
+	it('should pass correct options to CSLookup', () => {
+		const uut = shallow(
+			<CSFormLookupField
+				fieldToBeDisplayed={fieldToBeDisplayed}
+				fieldType={fieldType}
+				label={label}
+				name={name}
+				mode={modeClient}
+				columns={columns}
+				options={lookupOptions}
+			/>,
+		);
+		const lookup = uut.find('CSLookup');
+		expect(JSON.stringify(lookup.prop('options'))).toBe(JSON.stringify(lookupOptions));
+	});
+
+	it('should pass correct loading value to CSLookup', () => {
+		const uut = shallow(
+			<CSFormLookupField
+				fieldToBeDisplayed={fieldToBeDisplayed}
+				fieldType={fieldType}
+				label={label}
+				name={name}
+				mode={modeClient}
+				columns={columns}
+				options={lookupOptions}
+				loading
+			/>,
+		);
+		const lookup = uut.find('CSLookup');
+		expect(lookup.prop('loading')).toBeTruthy();
+	});
+
+	it('should pass correct searchBy value to CSLookup', () => {
+		const searchBy = ['Account'];
+		const uut = shallow(
+			<CSFormLookupField
+				fieldToBeDisplayed={fieldToBeDisplayed}
+				fieldType={fieldType}
+				label={label}
+				name={name}
+				mode={modeClient}
+				columns={columns}
+				options={lookupOptions}
+				searchBy={searchBy}
+			/>,
+		);
+		const lookup = uut.find('CSLookup');
+		expect(lookup.prop('searchBy')).toBe(searchBy);
+	});
+
+	// Server mode props
+	it('should call fetchOptions', () => {
+		const fetchOptionsMock = jest.fn();
+		const uut = shallow(
+			<CSFormLookupField
+				fieldToBeDisplayed={fieldToBeDisplayed}
+				fieldType={fieldType}
+				label={label}
+				name={name}
+				mode={modeServer}
+				columns={columns}
+				fetchOptions={fetchOptionsMock}
+				pageSize={pageSize}
+			/>,
+		);
+		const lookup = uut.find('CSLookup');
+		(lookup.props() as any).fetchOptions();
+		expect(fetchOptionsMock).toHaveBeenCalled();
+	});
+	it('should pass correct infiniteScroll value to CSLookup', () => {
+		const uut = shallow(
+			<CSFormLookupField
+				fieldToBeDisplayed={fieldToBeDisplayed}
+				fieldType={fieldType}
+				label={label}
+				name={name}
+				mode={modeServer}
+				columns={columns}
+				fetchOptions={fetchOptions}
+				pageSize={pageSize}
+				infiniteScroll
+			/>,
+		);
+		const lookup = uut.find('CSLookup');
+		expect(lookup.prop('infiniteScroll')).toBeTruthy();
+	});
+
+	it('should pass correct minTermLength value to CSLookup', () => {
+		const minTermLength = 3;
+		const uut = shallow(
+			<CSFormLookupField
+				fieldToBeDisplayed={fieldToBeDisplayed}
+				fieldType={fieldType}
+				label={label}
+				name={name}
+				mode={modeServer}
+				columns={columns}
+				fetchOptions={fetchOptions}
+				pageSize={pageSize}
+				minTermLength={minTermLength}
+			/>,
+		);
+		const lookup = uut.find('CSLookup');
+		expect(lookup.prop('minTermLength')).toBe(minTermLength);
+	});
+
+	it('should pass correct pageSize value to CSLookup', () => {
+		const uut = shallow(
+			<CSFormLookupField
+				fieldToBeDisplayed={fieldToBeDisplayed}
+				fieldType={fieldType}
+				label={label}
+				name={name}
+				mode={modeServer}
+				columns={columns}
+				fetchOptions={fetchOptions}
+				pageSize={pageSize}
+			/>,
+		);
+		const lookup = uut.find('CSLookup');
+		expect(lookup.prop('pageSize')).toBe(pageSize);
+	});
+});
--- cs-form-v2/src/form-fields/CSFormLookupField.tsx
+++ cs-form-v2/src/form-fields/CSFormLookupField.tsx
@@ -2,30 +2,32 @@ import React from 'react';
 import { CSLookup } from '@cloudsense/cs-ui-components';
 import { CSFormLookupFieldProps } from '../types/cs-form-field-types';
 
-const CSFormLookupField = ({ fieldType, ...props }: CSFormLookupFieldProps) => {
-	const { onBlur, onChange, styleClass } = props;
-
-	return (
-		<>
-			{props.mode === 'client'
-				? (
-					<CSLookup
-						onSelectChange={(value: any) => onChange(value)}
-						onBlur={(value: any) => onBlur(value)}
-						className={styleClass}
-						{...props}
-					/>
-				)
-				: (
-					<CSLookup
-						onSelectChange={(value: any) => onChange(value)}
-						onBlur={(value: any) => onBlur(value)}
-						className={styleClass}
-						{...props}
-					/>
-				)}
-		</>
-	);
-};
+const CSFormLookupField = ({
+	fieldType,
+	onBlur,
+	onChange,
+	styleClass,
+	...props
+}: CSFormLookupFieldProps) => (
+	<>
+		{props.mode === 'client'
+			? (
+				<CSLookup
+					onSelectChange={(value: any) => onChange(value)}
+					onBlur={(value: any) => onBlur(value)}
+					className={styleClass}
+					{...props}
+				/>
+			)
+			: (
+				<CSLookup
+					onSelectChange={(value: any) => onChange(value)}
+					onBlur={(value: any) => onBlur(value)}
+					className={styleClass}
+					{...props}
+				/>
+			)}
+	</>
+);
 
 export default CSFormLookupField;
