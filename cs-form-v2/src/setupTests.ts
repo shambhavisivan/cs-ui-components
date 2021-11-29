74597ed409c89ee80b0e4b92a19015078ffb22db
csleonspanic <leon.spanic@cloudsensesolutions.com>
2021-11-29 18:00:19 +0100

    [cs-ui][cs-form-v2] Updated package versions, removed jest-dom package
    Change-Id: I354b8ec87b0a47c485dec191658af4b3868ef851

--- cs-form-v2/src/setupTests.ts
+++ cs-form-v2/src/setupTests.ts
@@ -1,6 +1,5 @@
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import '@testing-library/jest-dom';
 import crypto from 'crypto';
 
 Object.defineProperty(global, 'crypto', {
