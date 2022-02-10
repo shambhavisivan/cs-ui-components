871226e6ec92aff3c7d2a358a47de6fdedeceae6
Sathyanarayanan <sathyanarayanan.somasundaram@cloudsense.com>
2022-02-10 17:37:01 +0530

    [cs-demo] CSAlertPreview - Added link to all props in prop table
    Change-Id: I8fff509c82744c24ff074ddf4f3e1b3513b28c01

--- cs-ui-demo/src/previews/cs-ui/components/cs-alert/cs-alert-props.ts
+++ cs-ui-demo/src/previews/cs-ui/components/cs-alert/cs-alert-props.ts
@@ -4,6 +4,7 @@ export default {
 	data: [
 		{
 			name: 'variant',
+			link: '/cs-ui/alert#variants',
 			required: true,
 			customTypes: {
 				name: 'CSAlertVariant',
@@ -17,20 +18,24 @@ export default {
 			description: 'Set the color and icon variant of the alert.'
 		}, {
 			name: 'closeButton',
+			link: '/cs-ui/alert#closing',
 			types: 'boolean',
 			default: 'false',
 			description: 'Show the close button.'
 		}, {
 			name: 'iconHidden',
+			link: '/cs-ui/alert#icons',
 			types: 'boolean',
 			default: 'false',
 			description: 'Show or hide the icon.'
 		}, {
 			name: 'iconName',
+			link: '/cs-ui/alert#icons',
 			types: 'string',
 			description: 'Override the default icon defined by the variant.'
 		}, {
 			name: 'iconOrigin',
+			link: '/cs-ui/alert#icons',
 			customTypes: {
 				name: 'CSAlertIconOrigin',
 				types: [`'slds'`, `'cs'`]
@@ -39,10 +44,12 @@ export default {
 			description: 'Select whether the Salesforce or the CloudSense icon set should be used.'
 		}, {
 			name: 'onClose',
+			link: '/cs-ui/alert#closing',
 			types: '(event) => void',
 			description: 'Handler method for closing the alert.'
 		}, {
 			name: 'styleFormat',
+			link: '/cs-ui/alert#emphasis',
 			customTypes: {
 				name: 'CSAlertStyleFormat',
 				types: [`'default'`, `'scoped'`]
@@ -51,6 +58,7 @@ export default {
 			description: 'Determine size of alert and icons.'
 		}, {
 			name: 'styleType',
+			link: '/cs-ui/alert#styling',
 			customTypes: {
 				name: 'CSAlertStyleType',
 				types: [`'default'`, `'light'`]
@@ -59,10 +67,12 @@ export default {
 			description: 'Determine style of text and background.'
 		}, {
 			name: 'text',
+			link: '/cs-ui/alert#text',
 			types: ['string', 'Array<string>'],
 			description: 'Set textual content of the alert.'
 		}, {
 			name: 'textAlign',
+			link: '/cs-ui/alert#alignment',
 			customTypes: {
 				name: 'CSAlertTextAlign',
 				types: [`'left'`, `'center'`]
@@ -71,14 +81,17 @@ export default {
 			description: 'Align text inside the alert.'
 		}, {
 			name: 'id',
+			link: '/cs-ui/alert#ids-&-classes',
 			types: 'string',
 			description: 'Set the ID for the alert.'
 		}, {
 			name: 'className',
+			link: '/cs-ui/alert#ids-&-classes',
 			types: 'string',
 			description: 'Apply custom CSS classes to the alert.'
 		}, {
 			name: 'children',
+			link: '/cs-ui/alert#children',
 			types: 'any',
 			description: 'This component supports custom content passed as children.'
 		}, {
