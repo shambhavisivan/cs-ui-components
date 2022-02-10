e5252f92381753b94b97a3bb1b1bbbfd3253a2eb
Simon East <simon.east@cloudsensesolutions.com>
2022-02-10 11:46:58 +0000

    [cs-demo] Fixed CSIcon alignment in CSDTable
    Change-Id: I40b0eb5d682352af6b92df8cd94774332535c268

--- cs-ui-demo/src/previews/helpers/getTypes.tsx
+++ cs-ui-demo/src/previews/helpers/getTypes.tsx
@@ -10,6 +10,7 @@ const getTypes = (types?: string | Array<string>) => {
 	return types.map((type: string, i) => (
 		<React.Fragment key={type}>
 			<code className="csd-inline-code" title={type}>{type}</code>
+			{/* Line below needs removing once all previews are moved over to new format */}
 			{i === types.length - 1 || <br />}
 		</React.Fragment>
 	));
