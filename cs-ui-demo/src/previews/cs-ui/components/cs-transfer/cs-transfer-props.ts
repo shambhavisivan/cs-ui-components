016803b37e74931b6f98f0458601201653a9ae76
Sathyanarayanan <sathyanarayanan.somasundaram@cloudsense.com>
2022-03-01 16:52:45 +0530

    [cs-demo] CSTransfer - Added link to all props in prop table
    Change-Id: I352b32bd9af7d6a9a48babc37c72a6eaad39a682

--- cs-ui-demo/src/previews/cs-ui/components/cs-transfer/cs-transfer-props.ts
+++ cs-ui-demo/src/previews/cs-ui/components/cs-transfer/cs-transfer-props.ts
@@ -4,6 +4,7 @@ export default {
 	data: [
 		{
 			name: 'items',
+			link: '/cs-ui/transfer#base-usage',
 			required: true,
 			customTypes: {
 				name: 'Array<CSTransferItemsType>',
@@ -12,16 +13,19 @@ export default {
 			description: 'Define an array of objects to be used as a data source. The object accepts key and label attributes. In case the key is within targetKeys, that item will be displayed in the target list.'
 		}, {
 			name: 'sourceLabel',
+			link: '/cs-ui/transfer#label-options',
 			required: true,
 			types: 'string',
 			description: 'Set the source list label.'
 		}, {
 			name: 'targetLabel',
+			link: '/cs-ui/transfer#label-options',
 			required: true,
 			types: 'string',
 			description: 'Set the target list label.'
 		}, {
 			name: 'helpText',
+			link: '/cs-ui/transfer#label-options',
 			customTypes: {
 				name: 'CSTransferHelpText',
 				types: 'object'
@@ -29,29 +33,35 @@ export default {
 			description: 'Set the text to be displayed in the transfer lists tooltip.'
 		}, {
 			name: 'onTransfer',
+			link: '/cs-ui/transfer#transferring-items',
 			types: '(key: ReactText | Array<ReactText>) => void',
 			description: 'Handler method for the change event.'
 		}, {
 			name: 'oneWay',
+			link: '/cs-ui/transfer#left-to-right-transfers',
 			types: 'boolean',
 			default: 'false',
 			description: 'Only allow left-to-right transfers.'
 		}, {
 			name: 'searchable',
+			link: '/cs-ui/transfer#search-for-items',
 			types: 'boolean',
 			default: 'false',
 			description: 'Render a search bar for both lists.'
 		}, {
 			name: 'selectAll',
+			link: '/cs-ui/transfer#select-all-items',
 			types: 'boolean',
 			default: 'false',
 			description: 'Render a checkbox to select all items in a list. It can be only used with the check-list variant.'
 		}, {
 			name: 'targetKeys',
+			link: '/cs-ui/transfer#data-management',
 			types: 'Array<ReactText>',
 			description: 'An array of keys corresponding to selected transfer items.'
 		}, {
 			name: 'variant',
+			link: '/cs-ui/transfer#styling',
 			customTypes: {
 				name: 'CSTransferVariant',
 				types: [`'simple-list'`, `'check-list'`]
@@ -60,10 +70,12 @@ export default {
 			description: 'Set a transfer list variant.'
 		}, {
 			name: 'id',
+			link: '/cs-ui/transfer#ids-&-classes',
 			types: 'string',
 			description: 'Set the ID for the transfer.'
 		}, {
 			name: 'className',
+			link: '/cs-ui/transfer#ids-&-classes',
 			types: 'string',
 			description: 'Apply custom CSS classes to the transfer.'
 		}, {
