9aafbe3d3e5a509a945488c97cee27b0058a722d
Dario Šehović <dario.sehovic@cloudsense.com>
2022-04-28 12:27:26 +0200

    Updated build.sh
    Change-Id: Ief84781f7551acf18249bf564acaf44081ef6d9e

--- build.sh
+++ build.sh
@@ -9,68 +9,70 @@ changedFiles () {
 npm install -g yarn;
 
 if changedFiles | grep -q -e '^cs-ui-components' -e '^cs-form-v2' -e '^cs-ui-demo' ; then
-      echo "Installing packages (cs-ui, cs-form-v2, cs-ui-demo)"
-      yarn install --ignore-engines;
-      echo "Finished installing packages"
+	echo "Installing packages (cs-ui-components, cs-form-v2, cs-demo)"
+	yarn install --ignore-engines;
+	echo "Finished installing packages"
 fi
 
 if changedFiles | grep -q '^cs-ui-components'; then
-    echo "Validating CS UI Components"
-    cd cs-ui-components;
-    echo "Running CS UI Components Lint"
-    yarn lint;
-    echo "Building CS UI Components"
-    yarn build;
-    echo "Running CS UI Components Tests"
-    yarn test;
-    echo "Finished validating CS UI Components"
-    cd ..;
+	echo "Validating cs-ui-components"
+	cd cs-ui-components;
+	echo "Linting cs-ui-components"
+	yarn lint;
+	echo "Running cs-ui-components tests"
+	yarn test;
+	echo "Building cs-ui-components"
+	yarn build;
+	echo "Finished validating cs-ui-components"
+	cd ..;
 fi
 
 if changedFiles | grep -q '^cs-ui-demo'; then
-    echo "Validating CS UI Demo"
-    cd cs-ui-demo;
-    echo "Running CS UI Demo Lint"
-    yarn lint;
-    echo "Finished validating CS UI Demo"
-    cd ..;
+	echo "Validating cs-demo"
+	cd cs-ui-demo;
+	echo "Linting cs-demo"
+	yarn lint;
+	echo "Finished validating cs-demo"
+	cd ..;
 fi
 
-#if changedFiles | grep -q '^cs-form'; then
-#    echo "Validating cs-form"
-#    cd cs-form;
-#    npm ci;
-#    echo "Running cs-form Tests"
-#    npm t;
-#    echo "Building cs-form"
-#    npm run build;
-#    echo "Finished validating cs-form"
-#    cd ..;
-#fi
+if changedFiles | grep -q '^cs-form/'; then
+	echo "Validating cs-form"
+	cd cs-form;
+	echo "Installing cs-form packages"
+	npm ci;
+	echo "Running cs-form tests"
+	npm t;
+	echo "Building cs-form"
+	npm run build;
+	echo "Finished validating cs-form"
+	cd ..;
+fi
 
 if changedFiles | grep -q '^cs-grid'; then
-    echo "Validating cs-grid"
-    cd cs-grid;
-    npm ci;
-    echo "Running cs-grid Tests"
-    npm t;
-    echo "Running cs-grid Lint"
-    npm run lint;
-    echo "Building cs-grid"
-    npm run build;
-    echo "Finished validating cs-grid"
-    cd ..;
+	echo "Validating cs-grid"
+	cd cs-grid;
+	echo "Installing cs-grid packages"
+	npm ci;
+	echo "Linting cs-grid"
+	npm run lint;
+	echo "Running cs-grid tests"
+	npm t;
+	echo "Building cs-grid"
+	npm run build;
+	echo "Finished validating cs-grid"
+	cd ..;
 fi
 
 if changedFiles | grep -q '^cs-form-v2'; then
-    echo "Validating cs-form-v2"
-    cd cs-form-v2;
-    echo "Running cs-form-v2 Lint"
-    yarn lint;
-    echo "Building cs-form-v2"
-    yarn build;
-    echo "Running cs-form-v2 Tests"
-    yarn test;
-    echo "Finished validating cs-form-v2"
-    cd ..;
+	echo "Validating cs-form-v2"
+	cd cs-form-v2;
+	echo "Linting cs-form-v2"
+	yarn lint;
+	echo "Running cs-form-v2 tests"
+	yarn test;
+	echo "Building cs-form-v2"
+	yarn build;
+	echo "Finished validating cs-form-v2"
+	cd ..;
 fi
